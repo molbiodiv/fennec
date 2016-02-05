d4ea19318f97b44d582d498e875e7b3729385df6
Markus Ankenbrand <markus@ankenbrand.me>
2016-02-05 14:00:37 +0100

    Add share dir, set smarty paths

--- src/webroot/config.php
+++ src/webroot/config.php
@@ -2,4 +2,5 @@
     if (strpos(get_include_path(), '${share_path}')===FALSE){
         set_include_path(get_include_path().PATH_SEPARATOR.'${share_path}');
     }
+    define('SHARE_DIR', '${share_path}');
 ?>
