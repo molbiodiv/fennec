d4fa80040c174cc4121d344c7acfb5d60114fcd1
Markus Ankenbrand <markus@ankenbrand.me>
2016-02-05 12:02:11 +0100

    Add basic index.php that just loads a config file and smarty

---
+++ src/webroot/index.php
@@ -0,0 +1,16 @@
+<?php
+
+//config file
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
+
+//required libs
+require_once 'smarty/libs/Smarty.class.php';
+
+//initialize smarty
+$smarty = new Smarty();
+$smarty->left_delimiter = '{#';
+$smarty->right_delimiter = '#}';
+
+?>
+
+
