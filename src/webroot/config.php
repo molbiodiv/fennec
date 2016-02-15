3fdb8fe9d2a20334d10663c11f09c13279fa2bc9
Markus Ankenbrand <markus@ankenbrand.me>
2016-02-15 17:30:48 +0100

    Load user config file from webroot config.php

--- src/webroot/config.php
+++ src/webroot/config.php
@@ -2,6 +2,8 @@
     if (strpos(get_include_path(), '${share_path}')===FALSE){
         set_include_path(get_include_path().PATH_SEPARATOR.'${share_path}');
     }
+    require_once '${config_dir}/config.php';
+    
     define('SHARE_DIR', '${share_path}');
     define('VAR_DIR', '${var_path}');
 ?>
