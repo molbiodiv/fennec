2b752342c7326474a62e1ee64672b65557ea73d0
Markus Ankenbrand <markus@ankenbrand.me>
2016-02-16 15:03:17 +0100

    Add pear/log to install and include_path

--- src/webroot/config.php
+++ src/webroot/config.php
@@ -2,6 +2,9 @@
     if (strpos(get_include_path(), '${share_path}')===FALSE){
         set_include_path(get_include_path().PATH_SEPARATOR.'${share_path}');
     }
+    if (strpos(get_include_path(), '${share_path}/pear/log')===FALSE){
+        set_include_path(get_include_path().PATH_SEPARATOR.'${share_path}/pear/log');
+    }
     require_once '${config_dir}/config.php';
     
     define('SHARE_DIR', '${share_path}');
