620026e72db8868e12c96b6745cb46e6a7a73b04
Markus Ankenbrand <markus@ankenbrand.me>
2016-02-16 11:04:22 +0100

    Add db php class to establish database connection via loggedPDO

---
+++ src/webservice/db.php
@@ -0,0 +1,49 @@
+<?php
+
+global $db;
+$db = get_db_connection(DB_CONNSTR, DB_USERNAME, DB_PASSWORD);
+
+function connect_queue_db() {
+    return get_db_connection(QUEUE_DB_CONNSTR, QUEUE_DB_USERNAME, QUEUE_DB_PASSWORD);
+}
+
+/**
+ * connects to databse using $connstr, $username, $password
+ * if DEBUG is set to true, use loggedPDO, not PDO
+ * @param String $connstr
+ * @param String $username
+ * @param String $password
+ * @return \PDO
+ */
+function get_db_connection($connstr, $username, $password) {
+    try {
+        if (defined('DEBUG') && DEBUG) {
+            require_once 'phryneas/logged-pdo/PDO.php';
+            require_once 'phryneas/logged-pdo/Log_firebugJSON.php';
+
+            if (PHP_SAPI == 'cli') {
+                $logtype = 'console';
+            } else {
+                if (in_array('Content-type: application/json', headers_list())) {
+                    $logtype = 'console';
+                } else {
+                    $logtype = 'firebugJSON';
+                }
+            }
+            $logger = Log::factory($logtype, '', 'PDO');
+            $db = new \LoggedPDO\PDO($connstr, $username, $password, null, $logger);
+            //$db->log_replace_params = false;
+        } else {
+            $db = new PDO($connstr, $username, $password, array(PDO::ATTR_PERSISTENT => true, PDO::ATTR_EMULATE_PREPARES => false));
+        }
+        #usually stop execution on DB error
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        return $db;
+    } catch (\PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
+}
+
+?>
