7b0d8befde554c7b7bf30799a971c5a683bd9926
Markus Ankenbrand <markus@ankenbrand.me>
2016-02-17 14:23:42 +0100

    Remove require_once statements, obsolete due to autoloader

--- src/webroot/index.php
+++ src/webroot/index.php
@@ -3,9 +3,6 @@
 //config file
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
 
-//required libs
-require_once 'smarty/smarty/libs/Smarty.class.php';
-
 //initialize smarty
 $smarty = new Smarty();
 $smarty->setTemplateDir(SHARE_DIR . DIRECTORY_SEPARATOR . 'smarty' . DIRECTORY_SEPARATOR . 'templates');
