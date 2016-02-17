27ad4b644d855a20c91f58eb2e0ec9e722b1ccb1
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-02-17 13:19:20 +0100

    Add smarty variable to the title template

--- src/webroot/index.php
+++ src/webroot/index.php
@@ -24,13 +24,13 @@ switch ($page) {
     case 'organism':
         $smarty->assign('navbar_type', 'organism');
         $smarty->assign('sidebar_type', 'organism');
-        $smarty->assign('title', 'organism');
+        $smarty->assign('title', 'Organisms');
         $smarty->display('organism.tpl');
         die();
 }
 $smarty->assign('navbar_type', 'startpage');
 $smarty->assign('sidebar_type', 'startpage');
-$smarty->assign('title', 'startpage');
+$smarty->assign('title', 'Welcome');
 $smarty->display('startpage.tpl');
 
 /**
