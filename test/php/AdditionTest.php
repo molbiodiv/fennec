7eea4653176a9298a9b8ba823e4ebae358d3392e
Markus Ankenbrand <markus@ankenbrand.me>
2016-02-18 11:27:29 +0100

    Add functional AdditionTest, usable with phpunit --includepath

---
+++ test/php/AdditionTest.php
@@ -0,0 +1,16 @@
+<?php
+class AdditionTest extends PHPUnit_Framework_TestCase
+{
+    public function testAddition()
+    {
+        include_once 'Addition.php';
+        $add = new Addition();
+        $this->assertEquals(0, $add->add(0,0));
+        $this->assertEquals(1, $add->add(0,1));
+        $this->assertEquals(1, $add->add(1,0));
+        $this->assertEquals(2, $add->add(1,1));
+        $this->assertEquals(3, $add->add(1,2));
+
+    }
+}
+?>
