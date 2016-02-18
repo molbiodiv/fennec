024f843a48cbe2a3f332d973e13446088680888f
Markus Ankenbrand <markus@ankenbrand.me>
2016-02-18 14:07:34 +0100

    Add real test case to OrganismsTest

--- test/php/OrganismsTest.php
+++ test/php/OrganismsTest.php
@@ -1,18 +1,16 @@
 <?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
+
 class OrganismsTest extends PHPUnit_Framework_TestCase
 {
     public function testPushAndPop()
     {
         list($service) = \WebService::factory('listing/Organisms');
-        $stack = array();
-        $this->assertEquals(0, count($stack));
+        $results = ($service->execute(array('ids' => array(1))));
+        //var_dump($results);
+        
+        $this->assertEquals(1, $results[0]["organism_id"]);
 
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack)-1]);
-        $this->assertEquals(1, count($stack));
-
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
     }
 }
 ?>
