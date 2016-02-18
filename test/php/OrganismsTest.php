0c278ae36908585bf29540bea989bb596da7a080
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-02-18 15:22:48 +0100

    Add test for new webservice getOrganismAbbrById and it fails

--- test/php/OrganismsTest.php
+++ test/php/OrganismsTest.php
@@ -3,13 +3,21 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
 
 class OrganismsTest extends PHPUnit_Framework_TestCase
 {
-    public function testPushAndPop()
+    public function testExecute()
     {
         list($service) = \WebService::factory('listing/Organisms');
         $results = ($service->execute(array('ids' => array(1))));
-        //var_dump($results);
-        
         $this->assertEquals(1, $results[0]["organism_id"]);
+
+    }
+    
+    public function testGetOrganismAbbr()
+    {
+        list($service) = \WebService::factory('listing/Organisms');
+        $results = ($service->getOrganismAbbr(array('ids' => array(1))));
+        var_dump($results);
+        
+        $this->assertEquals("H.sapiens", $results[0]["abbreviation"]);
 
     }
 }
