2cdbcac30910d9aaf066cf2c78a5544d3ca9a5f7
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-02-22 11:45:12 +0100

    Add plugins directory with call_webservice plugin

---
+++ src/smarty/plugins/function.call_webservice.php
@@ -0,0 +1,10 @@
+<?php
+function smarty_function_call_webservice($params, &$smarty) {
+    $service = WebService::factory($params['path']);
+    if ($service[0] != null) {
+        $ret = $service[0]->execute($params['data']);
+        $smarty->assign($params['assign'], $ret);
+    }
+}
+?>
+
