2bc383572797e9c8d4554795c7663e4eacecef9b
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-02-29 16:39:47 +0100

    Add first test for testing the organism details

--- test/php/OrganismDetailsTest.php
+++ test/php/OrganismDetailsTest.php
@@ -1,24 +1,14 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
 
-class OrganismsTest extends PHPUnit_Framework_TestCase
+class OrganismsDetailsTest extends PHPUnit_Framework_TestCase
 {
     public function testExecute()
     {
-        list($service) = \WebService::factory('listing/Organisms');
+        list($service) = \WebService::factory('listing/Details');
         
-        $results = ($service->execute(array('limit' => 2)));
-        $this->assertEquals(2, count($results));
-        
-        $results = ($service->execute(array()));
-        $this->assertEquals(5, count($results));
-        
-        $organism = 'Thermopsis';
-        $results = ($service->execute(array('limit' => 1, 'search' => $organism)));
-        $this->assertEquals(1, count($results));
-        $pos = strpos($results[0]['scientific_name'], $organism);
-        $this->assertEquals(0, $pos);
-
+        $results = ($service->execute(array('id' => 20)));
+        $this->assertEquals(2, $results['organism_id']);
     }
 }
 ?>
