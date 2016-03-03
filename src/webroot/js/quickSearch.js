cf667f0e5f33bad81f12f5ac783c0be193a94b55
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-03 16:08:23 +0100

    Change rewrite rule for organism details page

--- src/webroot/js/quickSearch.js
+++ src/webroot/js/quickSearch.js
@@ -19,7 +19,7 @@
 
 $("#search_organism").data("ui-autocomplete")._renderItem = function (ul, item) {
     var li = $("<li>")
-            .append("<a href='"+WebRoot+"/details/byId/"+item.organism_id+"' class='fancybox' data-fancybox-type='ajax'><span style='display:inline-block; width: 100%; font-style: italic;'>" + item.scientific_name + "</span><span style='color: #338C8C'>" + item.rank + "</span></a>")
+            .append("<a href='"+WebRoot+"/organism/details/byId/"+item.organism_id+"' class='fancybox' data-fancybox-type='ajax'><span style='display:inline-block; width: 100%; font-style: italic;'>" + item.scientific_name + "</span><span style='color: #338C8C'>" + item.rank + "</span></a>")
             .appendTo(ul);
     return li;
 };
