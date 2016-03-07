0c591a8d6d661c3f31d977102ba16de29979c0dd
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-07 14:53:30 +0100

    Draw interaction circles and add tooltips to them via d3

--- src/webroot/js/interactiveTraitBrowse.js
+++ src/webroot/js/interactiveTraitBrowse.js
@@ -1,6 +1,124 @@
-$("#circle1").click(function(){
-    alert("circle1");
-});
+var interactiveBrowse  = d3.select("#interactiveBrowse");
+var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
 
+var circle = interactiveBrowse.append("circle")
+        .attr("cx", 100)
+        .attr("cy", 700)
+        .attr("r", 10)
+        .attr("id", "desert")
+        .style("opacity", 1)
+        .style("fill", "red")
+        .on("mouseover", function(){
+            alert(d3.select(this).attr("id"));
+        });
+        
+var circle = interactiveBrowse.append("circle")
+        .attr("cx", 400)
+        .attr("cy", 525)
+        .attr("r", 10)
+        .attr("id", "salt water")
+        .style("opacity", 1)
+        .style("fill", "red")
+        .on("mouseover", function(){
+            alert(d3.select(this).attr("id"));
+        });
+        
+var circle = interactiveBrowse.append("circle")
+        .attr("cx", 800)
+        .attr("cy", 470)
+        .attr("r", 10)
+        .attr("id", "fresh water")
+        .style("opacity", 1)
+        .style("fill", "red")
+        .on("mouseover", function(){
+            alert(d3.select(this).attr("id"));
+        });
+        
+var circle = interactiveBrowse.append("circle")
+        .attr("cx", 700)
+        .attr("cy", 380)
+        .attr("r", 10)
+        .attr("id", "forest")
+        .style("opacity", 1)
+        .style("fill", "red")
+        .on("mouseover", function(){
+            alert(d3.select(this).attr("id"));
+        });
+        
+var circle = interactiveBrowse.append("circle")
+        .attr("cx", 750)
+        .attr("cy", 680)
+        .attr("r", 10)
+        .attr("id", "grassland")
+        .style("opacity", 1)
+        .style("fill", "red")
+        .on("mouseover", function(){
+            alert(d3.select(this).attr("id"));
+        });
+        
+var circle = interactiveBrowse.append("circle")
+        .attr("cx", 900)
+        .attr("cy", 90)
+        .attr("r", 10)
+        .attr("id", "mountain")
+        .style("opacity", 1)
+        .style("fill", "red")
+        .on("mouseover", function(){
+            alert(d3.select(this).attr("id"));
+        });
+        
+var circle = interactiveBrowse.append("circle")
+        .attr("cx", 300)
+        .attr("cy", 220)
+        .attr("r", 10)
+        .attr("id", "coast")
+        .style("opacity", 1)
+        .style("fill", "red")
+        .on("mouseover", function(){
+            alert(d3.select(this).attr("id"));
+        });
+        
+var circle = interactiveBrowse.append("circle")
+        .attr("cx", 250)
+        .attr("cy", 50)
+        .attr("r", 10)
+        .attr("id", "sky")
+        .style("opacity", 1)
+        .style("fill", "red")
+        .on("mouseover", function(){
+            add_Tooltip(d3.select(this).attr("id"));
+        })
+        .on("mouseout", function(){
+            remove_Tooltip();
+        });
+        
+var circle = interactiveBrowse.append("circle")
+        .attr("cx", 600)
+        .attr("cy", 175)
+        .attr("r", 10)
+        .attr("id", "snow and ice region")
+        .style("opacity", 1)
+        .style("fill", "red")
+        .on("mouseover", function(){
+            add_Tooltip(d3.select(this).attr("id"));
+        })
+        .on("mouseout", function(){
+            remove_Tooltip();
+        });
+            
+function add_Tooltip(text){
+    div.transition()
+        .duration(200)
+        .style("opacity", .9)
+        .text(text)
+        .style("left", (d3.event.pageX) + "px")     
+        .style("top", (d3.event.pageY - 28) + "px"); 
+}
 
-
+function remove_Tooltip(){
+    div.transition()
+        .duration(200)
+        .style("opacity", 0);
+}
