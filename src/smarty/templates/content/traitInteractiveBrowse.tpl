c4c6d7617bb63fb5d5adf424a28317d776736d1d
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-08 07:51:42 +0100

    Add smarty variable for making the interactive browse more dynamic

--- src/smarty/templates/content/traitInteractiveBrowse.tpl
+++ src/smarty/templates/content/traitInteractiveBrowse.tpl
@@ -2,5 +2,5 @@
     Not sure where to start? Explore our database with the following interactive search tool:
 </h3>
 <div class="row">
-    <svg width='1000' height='750' style='background-image: url("{#$WebRoot#}/css/img/traitOverview_contrast.png")' id='interactiveBrowse'/>
+    <svg width='1000' height='750' style='background-image: url("{#$WebRoot#}/css/img/traitInteractiveBrowse_{#$searchLevel#}.png")' id='interactiveBrowse_{#$searchLevel#}'/>
 </div>
--- src/webroot/js/interactiveTraitBrowse.js
+++ src/webroot/js/interactiveTraitBrowse.js
@@ -1,92 +1,15 @@
-var interactiveBrowse  = d3.select("#interactiveBrowse");
+var interactiveBrowse_start  = d3.select("#interactiveBrowse_start");
 var div = d3.select("body").append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
 
-var circle = interactiveBrowse.append("circle")
+var circle = interactiveBrowse_start.append("circle")
         .attr("cx", 100)
         .attr("cy", 700)
         .attr("r", 10)
         .attr("id", "desert")
         .style("opacity", 1)
-        .style("fill", "red")
-        .on("mouseover", function(){
-            alert(d3.select(this).attr("id"));
-        });
-        
-var circle = interactiveBrowse.append("circle")
-        .attr("cx", 400)
-        .attr("cy", 525)
-        .attr("r", 10)
-        .attr("id", "salt water")
-        .style("opacity", 1)
-        .style("fill", "red")
-        .on("mouseover", function(){
-            alert(d3.select(this).attr("id"));
-        });
-        
-var circle = interactiveBrowse.append("circle")
-        .attr("cx", 800)
-        .attr("cy", 470)
-        .attr("r", 10)
-        .attr("id", "fresh water")
-        .style("opacity", 1)
-        .style("fill", "red")
-        .on("mouseover", function(){
-            alert(d3.select(this).attr("id"));
-        });
-        
-var circle = interactiveBrowse.append("circle")
-        .attr("cx", 700)
-        .attr("cy", 380)
-        .attr("r", 10)
-        .attr("id", "forest")
-        .style("opacity", 1)
-        .style("fill", "red")
-        .on("mouseover", function(){
-            alert(d3.select(this).attr("id"));
-        });
-        
-var circle = interactiveBrowse.append("circle")
-        .attr("cx", 750)
-        .attr("cy", 680)
-        .attr("r", 10)
-        .attr("id", "grassland")
-        .style("opacity", 1)
-        .style("fill", "red")
-        .on("mouseover", function(){
-            alert(d3.select(this).attr("id"));
-        });
-        
-var circle = interactiveBrowse.append("circle")
-        .attr("cx", 900)
-        .attr("cy", 90)
-        .attr("r", 10)
-        .attr("id", "mountain")
-        .style("opacity", 1)
-        .style("fill", "red")
-        .on("mouseover", function(){
-            alert(d3.select(this).attr("id"));
-        });
-        
-var circle = interactiveBrowse.append("circle")
-        .attr("cx", 300)
-        .attr("cy", 220)
-        .attr("r", 10)
-        .attr("id", "coast")
-        .style("opacity", 1)
-        .style("fill", "red")
-        .on("mouseover", function(){
-            alert(d3.select(this).attr("id"));
-        });
-        
-var circle = interactiveBrowse.append("circle")
-        .attr("cx", 250)
-        .attr("cy", 50)
-        .attr("r", 10)
-        .attr("id", "sky")
-        .style("opacity", 1)
-        .style("fill", "red")
+        .style("fill", "#A90C0C")
         .on("mouseover", function(){
             add_Tooltip(d3.select(this).attr("id"));
         })
@@ -94,13 +17,114 @@ var circle = interactiveBrowse.append("circle")
             remove_Tooltip();
         });
         
-var circle = interactiveBrowse.append("circle")
+var circle = interactiveBrowse_start.append("circle")
+        .attr("cx", 400)
+        .attr("cy", 525)
+        .attr("r", 10)
+        .attr("id", "salt water")
+        .style("opacity", 1)
+        .style("fill", "#A90C0C")
+        .on("mouseover", function(){
+            add_Tooltip(d3.select(this).attr("id"));
+        })
+        .on("mouseout", function(){
+            remove_Tooltip();
+        });
+        
+var circle = interactiveBrowse_start.append("circle")
+        .attr("cx", 800)
+        .attr("cy", 470)
+        .attr("r", 10)
+        .attr("id", "fresh water")
+        .style("opacity", 1)
+        .style("fill", "#A90C0C")
+        .on("mouseover", function(){
+            add_Tooltip(d3.select(this).attr("id"));
+        })
+        .on("mouseout", function(){
+            remove_Tooltip();
+        });
+        
+var circle = interactiveBrowse_start_start.append("circle")
+        .attr("cx", 700)
+        .attr("cy", 380)
+        .attr("r", 10)
+        .attr("id", "forest")
+        .style("opacity", 1)
+        .style("fill", "#A90C0C")
+        .on("mouseover", function(){
+            add_Tooltip(d3.select(this).attr("id"));
+        })
+        .on("mouseout", function(){
+            remove_Tooltip();
+        })
+        .on("click", function(){
+            displayPage(d3.select(this).attr("id"));
+        });
+        
+var circle = interactiveBrowse_start.append("circle")
+        .attr("cx", 750)
+        .attr("cy", 680)
+        .attr("r", 10)
+        .attr("id", "grassland")
+        .style("opacity", 1)
+        .style("fill", "#A90C0C")
+        .on("mouseover", function(){
+            add_Tooltip(d3.select(this).attr("id"));
+        })
+        .on("mouseout", function(){
+            remove_Tooltip();
+        });
+        
+var circle = interactiveBrowse_start.append("circle")
+        .attr("cx", 900)
+        .attr("cy", 90)
+        .attr("r", 10)
+        .attr("id", "mountain")
+        .style("opacity", 1)
+        .style("fill", "#A90C0C")
+        .on("mouseover", function(){
+            add_Tooltip(d3.select(this).attr("id"));
+        })
+        .on("mouseout", function(){
+            remove_Tooltip();
+        });
+        
+var circle = interactiveBrowse_start.append("circle")
+        .attr("cx", 300)
+        .attr("cy", 220)
+        .attr("r", 10)
+        .attr("id", "coast")
+        .style("opacity", 1)
+        .style("fill", "#A90C0C")
+        .on("mouseover", function(){
+            add_Tooltip(d3.select(this).attr("id"));
+        })
+        .on("mouseout", function(){
+            remove_Tooltip();
+        });
+        
+var circle = interactiveBrowse_start.append("circle")
+        .attr("cx", 250)
+        .attr("cy", 50)
+        .attr("r", 10)
+        .attr("id", "sky")
+        .style("opacity", 1)
+        .style("fill", "#A90C0C")
+        .on("mouseover", function(){
+            add_Tooltip(d3.select(this).attr("id"));
+        })
+        .on("mouseout", function(){
+            remove_Tooltip();
+        });
+        
+var circle = interactiveBrowse_start.append("circle")
         .attr("cx", 600)
         .attr("cy", 175)
         .attr("r", 10)
         .attr("id", "snow and ice region")
         .style("opacity", 1)
-        .style("fill", "red")
+        .style("fill", "#A90C0C")
         .on("mouseover", function(){
             add_Tooltip(d3.select(this).attr("id"));
         })
