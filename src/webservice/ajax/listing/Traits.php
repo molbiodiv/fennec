536d2e28adcb4eabcd8f009bbc8cc99a0c046f6e
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-11 07:55:08 +0100

    Add simple sql query which returns all columns of trait_entry

--- src/webservice/ajax/listing/Traits.php
+++ src/webservice/ajax/listing/Traits.php
@@ -15,6 +15,24 @@ class Traits extends \WebService {
      * @returns array of traits
      */
     public function execute($querydata) {
+        global $db;
+        $data = array();
+        $search = "%%";
+        if(in_array('search', array_keys($querydata))){
+            $search = "%".$querydata['search']."%";
+        }
+        $query_get_traits = <<<EOF
+SELECT *
+    FROM trait_entry LIMIT 10;
+EOF;
+        $stm_get_traits = $db->prepare($query_get_traits);
+        $stm_get_traits->execute();
+        
+        while ($row = $stm_get_traits->fetch(PDO::FETCH_ASSOC)) {   
+            var_dump($row);
+        }
+        
+        return data;
     }
 }
 
