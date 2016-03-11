021aac6f095989fe4c7c4b1a3b9e27704a16a9eb
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-11 13:59:57 +0100

    Add one more test and fix wrong test statement

--- test/php/OrganismDetailsTest.php
+++ test/php/OrganismDetailsTest.php
@@ -1,13 +1,12 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
 
-class OrganismsDetailsTest extends PHPUnit_Framework_TestCase
+class DetailsTest extends PHPUnit_Framework_TestCase
 {
     public function testExecute()
     {
         list($service) = \WebService::factory('listing/Details');
-        
-        $results = ($service->execute(array('id' => 20)));
+        $results = ($service->execute(array('id' => 12)));
         $this->assertEquals(2, $results['organism_id']);
     }
 }
