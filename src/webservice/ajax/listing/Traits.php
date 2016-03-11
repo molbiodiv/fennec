3cc668a12596eab3f01309b2d1084cf6710058c2
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-11 07:48:03 +0100

    Add new Webservice class Traits and create simple body of function

---
+++ src/webservice/ajax/listing/Traits.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ajax\listing;
+
+use \PDO as PDO;
+
+/**
+ * Web Service.
+ * Returns Trait informatio
+ */
+class Traits extends \WebService {
+
+    /**
+     * @param $querydata[]
+     * @returns array of traits
+     */
+    public function execute($querydata) {
+    }
+}
+
+?>
+
