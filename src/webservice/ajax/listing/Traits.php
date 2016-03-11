c192142aaed5731ca4652daf2445e4882a0916ce
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-11 08:30:45 +0100

    Return trait names according to a search term

--- src/webservice/ajax/listing/Traits.php
+++ src/webservice/ajax/listing/Traits.php
@@ -16,23 +16,24 @@ class Traits extends \WebService {
      */
     public function execute($querydata) {
         global $db;
-        $data = array();
         $search = "%%";
         if(in_array('search', array_keys($querydata))){
             $search = "%".$querydata['search']."%";
         }
         $query_get_traits = <<<EOF
-SELECT name, tci.type_cvterm_id FROM (SELECT DISTINCT type_cvterm_id FROM trait_entry) AS tci, trait_cvterm WHERE tci.type_cvterm_id=trait_cvterm.trait_cvterm_id;
+SELECT name, tci.type_cvterm_id FROM (SELECT DISTINCT type_cvterm_id FROM trait_entry) AS tci, trait_cvterm WHERE tci.type_cvterm_id=trait_cvterm.trait_cvterm_id AND name LIKE :search;
 EOF;
         $stm_get_traits = $db->prepare($query_get_traits);
+        $stm_get_traits->bindValue('search', $search);
         $stm_get_traits->execute();
         
+        $data = array();
         while ($row = $stm_get_traits->fetch(PDO::FETCH_ASSOC)) {   
-            $result=array();
-            $result['name']=$row['name'];
-            $data[]=$result;
+            $result = array();
+            $result['name'] = $row['name'];
+            $data[] = $result;
         }
-        return data;
+        return $data;
     }
 }
 
