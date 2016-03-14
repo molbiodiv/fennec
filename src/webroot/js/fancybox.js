f9faf808f414ae53d372fe3ad5b4c6ca9b6e86c9
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-14 13:05:07 +0100

    Set size of fancy box

--- src/webroot/js/fancybox.js
+++ src/webroot/js/fancybox.js
@@ -2,6 +2,8 @@
  * Call the fancybox for displaying organism details in a lightbox
  */
     $(".fancybox").fancybox({
+        minWidth: 1000,
         maxWidth: 1000,
-        maxHeight: 800
+        maxHeight: 800,
+        minHeight: 800
     });
