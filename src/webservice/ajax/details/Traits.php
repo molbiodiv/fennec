12a1c1a09debb5a240f76568614a88986b6fbff3
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-14 13:06:11 +0100

    Add sql query which returns a specific trait accoring to its cvterm_id

--- src/webservice/ajax/details/Traits.php
+++ src/webservice/ajax/details/Traits.php
@@ -16,14 +16,14 @@ class Traits extends \WebService {
      */
     public function execute($querydata) {
         global $db;
-        $trait_cvterm_id = $querydata['trait_cvterm_id'];
-        $placeholders = implode(',', array_fill(0, count($trait_cvterm_id), '?'));
+        $type_cvterm_id = $querydata['type_cvterm_id'];
+        $placeholders = implode(',', array_fill(0, count($type_cvterm_id), '?'));
         $query_get_trait = <<<EOF
 SELECT *
     FROM trait_cvterm WHERE trait_cvterm_id IN ($placeholders)
 EOF;
         $stm_get_trait= $db->prepare($query_get_trait);
-        $stm_get_trait->execute(array($trait_cvterm_id));
+        $stm_get_trait->execute(array($type_cvterm_id));
 
         $result = array();
         while ($row = $stm_get_trait->fetch(PDO::FETCH_ASSOC)) {
