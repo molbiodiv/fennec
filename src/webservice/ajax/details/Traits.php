a9d5a9cb9ab9a377602b0790b2a5dab008bc5840
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-14 10:25:41 +0100

    Get simple data of trait via its cvterm_id

--- src/webservice/ajax/details/Traits.php
+++ src/webservice/ajax/details/Traits.php
@@ -15,7 +15,22 @@ class Traits extends \WebService {
      * @returns array of traits
      */
     public function execute($querydata) {
-        var_dump($querydata);
+        global $db;
+        $trait_cvterm_id = $querydata['trait_cvterm_id'];
+        $placeholders = implode(',', array_fill(0, count($trait_cvterm_id), '?'));
+        $query_get_trait = <<<EOF
+SELECT *
+    FROM trait_cvterm WHERE trait_cvterm_id IN ($placeholders)
+EOF;
+        $stm_get_trait= $db->prepare($query_get_trait);
+        $stm_get_trait->execute(array($trait_cvterm_id));
+
+        $result = array();
+        while ($row = $stm_get_trait->fetch(PDO::FETCH_ASSOC)) {
+            $result = $row;
+            
+        }
+        return $result;
     }
 }
 
