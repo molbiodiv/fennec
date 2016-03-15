3f6230939e5e37c38828d560a75193f84ffe95ad
Markus Ankenbrand <markus@ankenbrand.me>
2016-03-15 17:37:24 +0100

    Add backupGlobals disabled directive to use the global db variable

--- test/php/OrganismDetailsTest.php
+++ test/php/OrganismDetailsTest.php
@@ -1,13 +1,16 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
 
+/**
+ * @backupGlobals disabled
+ */
 class DetailsTest extends PHPUnit_Framework_TestCase
 {
     public function testExecute()
     {
         list($service) = \WebService::factory('listing/Details');
-        $results = ($service->execute(array('id' => 12)));
-        $this->assertEquals(2, $results['organism_id']);
+        $results = ($service->execute(array('id' => 13)));
+        $this->assertEquals(13, $results['organism_id']);
     }
 }
 ?>
