f0c5cc99b097c29e9f8cf20fd569827041673140
Markus Ankenbrand <markus@ankenbrand.me>
2016-03-15 18:15:27 +0100

    Add setupFixtures script, drops, creates and fills db from dump

---
+++ test/php/setupFixtures.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
+
+echo exec('PGPASSWORD='.DB_PASSWORD.' dropdb -U '.DB_USERNAME.' -h '.DB_HOST.' -p '.DB_PORT.' '.DB_DBNAME);
+echo exec('PGPASSWORD='.DB_PASSWORD.' createdb -U '.DB_USERNAME.' -h '.DB_HOST.' -p '.DB_PORT.' '.DB_DBNAME);
+echo exec('PGPASSWORD='.DB_PASSWORD.' xzcat '.__DIR__.'/chado_traits.sql.xz | psql -U '.DB_USERNAME.' -h '.DB_HOST.' -p '.DB_PORT.' -d '.DB_DBNAME);
+?>
