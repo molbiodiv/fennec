b3fb1cab263aa9539d4f87403568ca50648582f6
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-16 16:40:16 +0100

    Add functions for displaying pie chart and map

--- src/webroot/js/drawCharts.js
+++ src/webroot/js/drawCharts.js
@@ -19,5 +19,65 @@ function drawHistogram(){
     });
 }
 
+function drawPieChart(){
+    var cvterm_id = $("#cvterm_id").val();
+    $.ajax({
+        url: WebRoot.concat("/ajax/details/Traits"),
+        data: {type_cvterm_id: cvterm_id},
+        dataType: "json",
+        success: function (data) {
+            var plot = [{
+                    values: data['value']['frequency'],
+                    labels: data['value']['labels'],
+                    type: 'pie'
+            }];
+            var layout = {
+                height: 700,
+                width: 800
+            };
+            Plotly.newPlot('pieChart', plot, layout);
+        }
+    });
+}
+
+function drawMap(){
+    var cvterm_id = $("#cvterm_id").val();
+    $.ajax({
+        url: WebRoot.concat("/ajax/details/Traits"),
+        data: {type_cvterm_id: cvterm_id},
+        dataType: "json",
+        success: function (data) {
+//           csvMap = generateGeographicMap(data);
+            Plotly.d3.csv(WebRoot.concat("/test.csv"), function(err, rows){
+                function unpack(rows, key) {
+                    return rows.map(function(row) { return row[key]; });
+                }
+
+                var plot = [{
+                    type: 'choropleth',
+                    locationmode: 'country names',
+                    locations: unpack(rows, 'labels'),
+                    z: unpack(rows, 'frequency'),
+                    text: unpack(rows, 'labels'),
+                    autocolorscale: true
+                }];
+
+                var layout = {
+                  title: 'Geographic distribution',
+                  geo: {
+                      projection: {
+                          type: 'robinson'
+                      }
+                  },
+                  height: 700,
+                  width: 1000
+                };
+
+                Plotly.plot(map, plot, layout, {showLink: false});
+            });
+        }
+    });
+}
+
 
 
