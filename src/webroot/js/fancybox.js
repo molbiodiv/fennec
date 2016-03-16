09259e45888f52aaf3130a4ac565c2342c92c194
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-16 16:33:57 +0100

    Change size of fancybox alert

--- src/webroot/js/fancybox.js
+++ src/webroot/js/fancybox.js
@@ -2,8 +2,8 @@
  * Call the fancybox for displaying organism details in a lightbox
  */
     $(".fancybox").fancybox({
-        minWidth: 1000,
-        maxWidth: 1000,
+        minWidth: 1200,
+        maxWidth: 1200,
         maxHeight: 800,
         minHeight: 800
     });
