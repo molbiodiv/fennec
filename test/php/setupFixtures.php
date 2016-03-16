9d3ae679fbde9357497b225e8af98cee2a2f9342
Markus Ankenbrand <markus@ankenbrand.me>
2016-03-16 14:24:50 +0100

    Do dropdb only if db exists

--- test/php/setupFixtures.php
+++ test/php/setupFixtures.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . DIRECTORY_SEPARATOR . '../../vendor/autoload.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
 
-echo exec('PGPASSWORD='.DB_PASSWORD.' dropdb -U '.DB_USERNAME.' -h '.DB_HOST.' -p '.DB_PORT.' '.DB_DBNAME);
+echo exec('PGPASSWORD='.DB_PASSWORD.' dropdb --if-exists -U '.DB_USERNAME.' -h '.DB_HOST.' -p '.DB_PORT.' '.DB_DBNAME);
 echo exec('PGPASSWORD='.DB_PASSWORD.' createdb -U '.DB_USERNAME.' -h '.DB_HOST.' -p '.DB_PORT.' '.DB_DBNAME);
 echo exec('PGPASSWORD='.DB_PASSWORD.' xzcat '.__DIR__.'/chado_traits.sql.xz | psql -U '.DB_USERNAME.' -h '.DB_HOST.' -p '.DB_PORT.' -d '.DB_DBNAME);
 ?>
