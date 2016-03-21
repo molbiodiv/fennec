17fc26ce5f1bec0c8d3865ca3f657b89f372a7b3
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-21 09:56:04 +0100

    Remove non-used count and sort functions

--- src/webservice/ajax/listing/Traits.php
+++ src/webservice/ajax/listing/Traits.php
@@ -39,41 +39,10 @@ EOF;
             $result = array();
             $result['name'] = $row['name'];
             $result['type_cvterm_id'] = $row['type_cvterm_id'];
-            $result['frequency'] = $this->count_frequency_of_trait($row['type_cvterm_id']);
+            $result['frequency'] = $row['count'];
             $data[] = $result;
         }
-        usort($data, array($this, 'cmp'));
         return $data;
-    }
-    
-    /**
-     * This function counts the frequency of apperance of a specifi trait
-     * @param $type_cvterm_id Id of trait
-     * @returns the count of its appearance in the databse
-     */
-    private function count_frequency_of_trait($type_cvterm_id){
-        global $db;
-        $query_count_frequency = <<<EOF
-SELECT count(type_cvterm_id) FROM trait_entry WHERE type_cvterm_id = :type_cvterm_id
-EOF;
-        $stm_count_frequency = $db->prepare($query_count_frequency);
-        $stm_count_frequency->bindValue('type_cvterm_id', $type_cvterm_id);
-        $stm_count_frequency->execute();
-        
-        $result = $stm_count_frequency->fetch(PDO::FETCH_ASSOC);
-        return $result['count'];
-    }
-    
-    /**
-     * Functions which sorts the php result by its frequency
-     * @param type $a, $b Values of frequency
-     * @return int
-     */
-    public static function cmp($a, $b){
-        if($a['frequency'] == $b['frequency']){
-            return 0;
-        }
-        return ($a['frequency'] > $b['frequency']) ? -1 : 1;
     }
 }
 
