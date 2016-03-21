d88ab5d5d3359b89efb00a49f11358ae6dfb8b1c
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-21 10:49:55 +0100

    Add new webservice call for displaying all organisms to a specific trait

---
+++ src/webservice/ajax/details/Organisms_to_traits.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ajax\details;
+
+use \PDO as PDO;
+
+/**
+ * Web Service.
+ * Returns Organisms with given ids
+ */
+class Organisms extends \WebService {
+
+    /**
+     * @param $querydata[type_cvterm_id] array of trait type_cvterm_id
+     * @returns array of organisms accoring to a specific trait type
+     */
+    public function execute($querydata) {
+        global $db;
+        
+    }
+    
+}
+
+?>
+
