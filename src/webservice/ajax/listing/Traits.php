9dd2cf57de74d81077f81cc6b84d92e3cfa4cf15
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-21 09:46:42 +0100

    Change sql stm and sort the count of entrys

--- src/webservice/ajax/listing/Traits.php
+++ src/webservice/ajax/listing/Traits.php
@@ -16,15 +16,22 @@ class Traits extends \WebService {
      */
     public function execute($querydata) {
         global $db;
+        $limit = 5;
+        if(in_array('limit', array_keys($querydata))){
+            $limit = $querydata['limit'];
+        }
         $search = "%%";
         if(in_array('search', array_keys($querydata))){
             $search = "%".$querydata['search']."%";
         }
         $query_get_traits = <<<EOF
-SELECT name, tci.type_cvterm_id FROM (SELECT DISTINCT type_cvterm_id FROM trait_entry) AS tci, trait_cvterm WHERE tci.type_cvterm_id=trait_cvterm.trait_cvterm_id AND name LIKE :search;
+SELECT name, tci.type_cvterm_id, count 
+    FROM (SELECT type_cvterm_id, count(type_cvterm_id) AS count FROM trait_entry GROUP BY type_cvterm_id ORDER BY count DESC LIMIT :limit) AS tci, trait_cvterm 
+        WHERE tci.type_cvterm_id=trait_cvterm.trait_cvterm_id AND name LIKE :search             
 EOF;
         $stm_get_traits = $db->prepare($query_get_traits);
         $stm_get_traits->bindValue('search', $search);
+        $stm_get_traits->bindValue('limit', $limit);
         $stm_get_traits->execute();
         
         $data = array();
