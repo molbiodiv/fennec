226eefba5e15b2a0f778f44ac4d43b0d82168ff4
Markus Ankenbrand <markus@ankenbrand.me>
2016-03-21 08:18:09 +0100

    Update path to webservice in test

--- test/php/OrganismDetailsTest.php
+++ test/php/OrganismDetailsTest.php
@@ -8,7 +8,7 @@ class DetailsTest extends PHPUnit_Framework_TestCase
 {
     public function testExecute()
     {
-        list($service) = \WebService::factory('listing/Details');
+        list($service) = \WebService::factory('details/Organisms');
         $results = ($service->execute(array('id' => 13)));
         $this->assertEquals(13, $results['organism_id']);
     }
