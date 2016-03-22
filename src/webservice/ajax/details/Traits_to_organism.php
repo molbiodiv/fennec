85bfa25f093f40d44c6b5a380f4220b577e8805e
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-03-22 10:36:53 +0100

    Add new webservice class for getting all trait info to an organism by its id

---
+++ src/webservice/ajax/details/Traits_to_organism.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ajax\listing;
+
+use \PDO as PDO;
+
+/**
+ * Web Service.
+ * Returns Trait informatio
+ */
+class Traits_to_organism extends \WebService {
+
+    /**
+     * @param $querydata[]
+     * @returns array of traits
+     */
+    public function execute($querydata) {
+
+    }
+}
+
+?>
+
