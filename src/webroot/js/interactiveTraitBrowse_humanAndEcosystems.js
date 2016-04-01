78f00a05e7823cfbe183b35367273dbbdc672e1f
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-04-01 14:46:38 +0200

    Draw event box onto the image of this trait browse level

---
+++ src/webroot/js/interactiveTraitBrowse_humanAndEcosystems.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    var div = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
+    var interactiveBrowse_humanAndEcosystems = d3.select("#interactiveBrowse_humanAndEcosystems");
+
+    var toxicity = interactiveBrowse_humanAndEcosystems.append("rect")
+            .attr("x", 590)
+            .attr("y", 180)
+            .attr("width", 220)
+            .attr("height", 70)
+            .attr("id", "human/livestock toxicity")
+            .attr("type_cvterm_id", "480")
+            .style("opacity", 0.01)
+            .style("fill", "#fff")
+            .style("cursor", "pointer")
+            .on("click", function(){
+                displayPage(d3.select(this).attr("type_cvterm_id"));
+            });
+
+    function displayPage(traitId){
+        var resultPage =  WebRoot+"/trait/details/byId/"+traitId;
+        $.fancybox.open({
+           type: 'iframe',
+           href: resultPage,
+           minWidth: 1000,
+            maxWidth: 1000,
+            maxHeight: 800,
+            minHeight: 800
+        });
+    }
+});
