aeadb2b9780b86cc1772b6a377d901b15bca1e3d
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-04-01 14:18:21 +0200

    Add template for click event to overview img

--- src/webroot/js/interactiveTraitBrowse.js
+++ src/webroot/js/interactiveTraitBrowse.js
@@ -19,6 +19,26 @@ var ecology = interactiveBrowse_overview.append("rect")
         .on("click", function(){
             displayPage(d3.select(this).attr("id"));
         });
+        
+var humanEco = interactiveBrowse_overview.append("rect")
+        .attr("x", 500)
+        .attr("y", 430)
+        .attr("width", 600)
+        .attr("height", 280)
+        .attr("id", "humanAndEcosystems")
+        .attr("text", "Search for traits related to the relevance to humans and ecosystems")
+        .style("opacity", 0.01)
+        .style("fill", "#fff")
+        .style("cursor", "pointer")
+//        .on("mouseover", function(){
+//            add_Tooltip(d3.select(this).attr("text"));
+//        })
+        .on("mouseout", function(){
+            remove_Tooltip();
+        })
+        .on("click", function(){
+            displayPage(d3.select(this).attr("id"));
+        });
 
 function remove_Tooltip(){
     div.transition()
