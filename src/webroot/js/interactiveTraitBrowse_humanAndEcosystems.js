30ec015f75a79d5d7a9327b08daef0583a45869b
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-04-01 15:20:28 +0200

    Add more events to the image

--- src/webroot/js/interactiveTraitBrowse_humanAndEcosystems.js
+++ src/webroot/js/interactiveTraitBrowse_humanAndEcosystems.js
@@ -32,6 +32,62 @@ $(document).ready(function(){
             .on("click", function(){
                 displayPage(d3.select(this).attr("type_cvterm_id"));
             });
+            
+    var horticulture = interactiveBrowse_humanAndEcosystems.append("rect")
+            .attr("x", 150)
+            .attr("y", 400)
+            .attr("width", 180)
+            .attr("height", 50)
+            .attr("id", "horticulture")
+            .attr("type_cvterm_id", "458")
+            .style("opacity", 0.01)
+            .style("fill", "#fff")
+            .style("cursor", "pointer")
+            .on("click", function(){
+                displayPage(d3.select(this).attr("type_cvterm_id"));
+            });
+            
+    var commercialAvailability = interactiveBrowse_humanAndEcosystems.append("rect")
+            .attr("x", 250)
+            .attr("y", 60)
+            .attr("width", 180)
+            .attr("height", 70)
+            .attr("id", "commercial availability")
+            .attr("type_cvterm_id", "505")
+            .style("opacity", 0.01)
+            .style("fill", "#fff")
+            .style("cursor", "pointer")
+            .on("click", function(){
+                displayPage(d3.select(this).attr("type_cvterm_id"));
+            });
+            
+    var grazeAnimal = interactiveBrowse_humanAndEcosystems.append("rect")
+            .attr("x", 820)
+            .attr("y", 540)
+            .attr("width", 160)
+            .attr("height", 80)
+            .attr("id", "graze animal palatability")
+            .attr("type_cvterm_id", "1263")
+            .style("opacity", 0.01)
+            .style("fill", "#fff")
+            .style("cursor", "pointer")
+            .on("click", function(){
+                displayPage(d3.select(this).attr("type_cvterm_id"));
+            });
+            
+    var grainType = interactiveBrowse_humanAndEcosystems.append("rect")
+            .attr("x", 540)
+            .attr("y", 300)
+            .attr("width", 80)
+            .attr("height", 60)
+            .attr("id", "graze animal palatability")
+            .attr("type_cvterm_id", "528")
+            .style("opacity", 0.01)
+            .style("fill", "#fff")
+            .style("cursor", "pointer")
+            .on("click", function(){
+                displayPage(d3.select(this).attr("type_cvterm_id"));
+            });
 
     function displayPage(traitId){
         var resultPage =  WebRoot+"/trait/details/byId/"+traitId;
