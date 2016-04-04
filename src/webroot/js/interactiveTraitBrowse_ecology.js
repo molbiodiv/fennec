305be90ff5ee5863bdac607d0e39867ef365524c
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-04-04 10:26:25 +0200

    Add missing trait types to ecology level of interactive trait browse

--- src/webroot/js/interactiveTraitBrowse_ecology.js
+++ src/webroot/js/interactiveTraitBrowse_ecology.js
@@ -46,6 +46,62 @@ $(document).ready(function(){
             .on("click", function(){
                 displayPage(d3.select(this).attr("type_cvterm_id"));
             });
+            
+    var waterDepth = interactiveBrowse_ecology.append("rect")
+            .attr("x", 260)
+            .attr("y", 380)
+            .attr("width", 170)
+            .attr("height", 40)
+            .attr("id", "water depth")
+            .attr("type_cvterm_id", "17")
+            .style("opacity", 0.01)
+            .style("fill", "#fff")
+            .style("cursor", "pointer")
+            .on("click", function(){
+                displayPage(d3.select(this).attr("type_cvterm_id"));
+            });
+            
+    var lowTmpTolerance = interactiveBrowse_ecology.append("rect")
+            .attr("x", 30)
+            .attr("y", 50)
+            .attr("width", 200)
+            .attr("height", 80)
+            .attr("id", "low temperature tolerance")
+            .attr("type_cvterm_id", "436")
+            .style("opacity", 0.01)
+            .style("fill", "#fff")
+            .style("cursor", "pointer")
+            .on("click", function(){
+                displayPage(d3.select(this).attr("type_cvterm_id"));
+            });
+            
+    var calcTolerance = interactiveBrowse_ecology.append("rect")
+            .attr("x", 680)
+            .attr("y", 80)
+            .attr("width", 180)
+            .attr("height", 70)
+            .attr("id", "calcareous soil tolerance")
+            .attr("type_cvterm_id", "492")
+            .style("opacity", 0.01)
+            .style("fill", "#fff")
+            .style("cursor", "pointer")
+            .on("click", function(){
+                displayPage(d3.select(this).attr("type_cvterm_id"));
+            });
+            
+    var soilDepth = interactiveBrowse_ecology.append("rect")
+            .attr("x", 450)
+            .attr("y", 615)
+            .attr("width", 150)
+            .attr("height", 50)
+            .attr("id", "soil depth")
+            .attr("type_cvterm_id", "435")
+            .style("opacity", 0.01)
+            .style("fill", "#fff")
+            .style("cursor", "pointer")
+            .on("click", function(){
+                displayPage(d3.select(this).attr("type_cvterm_id"));
+            });
 
     function displayPage(traitId){
         var resultPage =  WebRoot+"/trait/details/byId/"+traitId;
