8d2bb5c6e79026f11c7121d7568a8a8f8e7817a6
Markus Ankenbrand <markus@ankenbrand.me>
2016-04-11 14:18:21 +0200

    Split long lines

--- test/php/setupFixtures.php
+++ test/php/setupFixtures.php
@@ -2,6 +2,19 @@
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
 
 $database = unserialize(DATABASE)['test'];
-echo exec('PGPASSWORD='.$database['DB_PASSWORD'].' dropdb --if-exists -U '.$database['DB_USERNAME'].' -h '.$database['DB_HOST'].' -p '.$database['DB_PORT'].' '.$database['DB_DBNAME']);
-echo exec('PGPASSWORD='.$database['DB_PASSWORD'].' createdb -U '.$database['DB_USERNAME'].' -h '.$database['DB_HOST'].' -p '.$database['DB_PORT'].' '.$database['DB_DBNAME']);
-echo exec('PGPASSWORD='.$database['DB_PASSWORD'].' xzcat '.__DIR__.'/chado_traits.sql.xz | psql -U '.$database['DB_USERNAME'].' -h '.$database['DB_HOST'].' -p '.$database['DB_PORT'].' -d '.$database['DB_DBNAME']);
+echo exec('PGPASSWORD='.$database['DB_PASSWORD'].
+        ' dropdb --if-exists -U '.$database['DB_USERNAME'].
+        ' -h '.$database['DB_HOST'].
+        ' -p '.$database['DB_PORT'].
+        ' '.$database['DB_DBNAME']);
+echo exec('PGPASSWORD='.$database['DB_PASSWORD'].
+        ' createdb -U '.$database['DB_USERNAME'].
+        ' -h '.$database['DB_HOST'].
+        ' -p '.$database['DB_PORT'].
+        ' '.$database['DB_DBNAME']);
+echo exec('PGPASSWORD='.$database['DB_PASSWORD'].
+        ' xzcat '.__DIR__.'/chado_traits.sql.xz | psql -U '.
+        $database['DB_USERNAME'].
+        ' -h '.$database['DB_HOST'].
+        ' -p '.$database['DB_PORT'].
+        ' -d '.$database['DB_DBNAME']);
