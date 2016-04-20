df0e18068232756e4fd77cd52080b21a07ae58f8
Markus Ankenbrand <markus@ankenbrand.me>
2016-04-20 15:06:08 +0200

    Add gulp apigen task

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,9 @@ var gulp = require('gulp');
 gulp.task('default', function() {
   // place code for your default task here
 });
+
+var apigen = require('gulp-apigen');
+
+gulp.task('apigen', function() {
+  gulp.src('apigen.neon').pipe(apigen('./vendor/bin/apigen'));
+});
