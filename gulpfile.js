462c4a8b0208956467597f1ded4550adc9c25039
Markus Ankenbrand <markus@ankenbrand.me>
2016-04-20 15:36:24 +0200

    Add gulp phpunit task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
+var apigen = require('gulp-apigen');
+var phpunit = require('gulp-phpunit');
+
+gulp.task('apigen', function() {
+  gulp.src('apigen.neon').pipe(apigen('./vendor/bin/apigen'));
+});
+
+gulp.task('phpunit', function() {
+  gulp.src('phpunit.xml').pipe(phpunit('./vendor/bin/phpunit'));
+});
 
 gulp.task('default', function() {
   // place code for your default task here
 });
 
-var apigen = require('gulp-apigen');
-
-gulp.task('apigen', function() {
-  gulp.src('apigen.neon').pipe(apigen('./vendor/bin/apigen'));
-});
