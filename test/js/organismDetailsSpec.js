9697b8d10a8312fb9e07009ce62a8e380e68bc76
Markus Ankenbrand <markus@ankenbrand.me>
2016-04-22 15:23:54 +0200

    Expand test case for getBestName

--- test/js/organismDetailsSpec.js
+++ test/js/organismDetailsSpec.js
@@ -1,6 +1,16 @@
 describe("Test the organismDetails file", function() {
 
-  it("Test for getBestName", function() {
+  it("Test for getBestName on empty set", function() {
+    expect(getBestName({})).toBe('');
+  });
+  it("Test for getBestName with only sciname", function() {
+    expect(getBestName({'scientificName': 'Mus musculus'})).toBe('Mus musculus');
+    expect(getBestName({'scientificName': 'Vulpes zerda', 'vernacularNames': []})).toBe('Vulpes zerda');
+  });
+  it("Test for getBestName with common names (no preffered name)", function() {
+    expect(getBestName({'scientificName': 'Vulpes zerda', 'vernacularNames': [{'language': 'en', 'vernacularName': 'Fennec'}]})).toBe('Fennec');
+  });
+  it("Test for getBestName with common names (preffered name)", function() {
     expect(getBestName({})).toBe('');
   });
 });
