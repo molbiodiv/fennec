7755fb999dcc3f2a328db29ae6ed006e07466d16
Markus Ankenbrand <markus@ankenbrand.me>
2016-04-22 12:07:15 +0200

    Add gulp-phpcs plugin and configure it

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var apigen = require('gulp-apigen');
 var phpunit = require('gulp-phpunit');
+var phpcs = require('gulp-phpcs');
 
 gulp.task('apigen', function() {
   gulp.src('apigen.neon').pipe(apigen('./vendor/bin/apigen'));
@@ -8,6 +9,12 @@ gulp.task('apigen', function() {
 
 gulp.task('phpunit', function() {
   gulp.src('phpunit.xml').pipe(phpunit('./vendor/bin/phpunit'));
+});
+
+gulp.task('phpcs', function () {
+  return gulp.src(['src/webservice/*.php','src/webservice/**/*.php'])
+             .pipe(phpcs({ bin: 'vendor/bin/phpcs', standard: 'PSR2' }))
+             .pipe(phpcs.reporter('log'));
 });
 
 gulp.task('default', function() {
