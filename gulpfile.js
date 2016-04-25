d67bbe3660a81d9617fbfa34baa40284b8588cf1
Markus Ankenbrand <markus@ankenbrand.me>
2016-04-25 14:57:09 +0200

    Add gulp-sass and gulp-sourcemaps. Fix #4

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
+'use strict';
+
 var gulp = require('gulp');
 var apigen = require('gulp-apigen');
 var phpunit = require('gulp-phpunit');
 var spawn = require('child_process').spawn;
 var jasmine = require('gulp-jasmine');
 var cover = require('gulp-coverage');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('apigen', function() {
   gulp.src('apigen.neon').pipe(apigen('./vendor/bin/apigen'));
@@ -32,6 +36,14 @@ gulp.task('jasmine', function() {
         .pipe(cover.gather())
         .pipe(cover.format())
         .pipe(gulp.dest('test/js/cover'));;
+});
+
+gulp.task('sass', function () {
+  return gulp.src('src/webroot/scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('.', {includeContent: false}))
+    .pipe(gulp.dest('src/webroot/css'));
 });
 
 gulp.task('default', function() {
