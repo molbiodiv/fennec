b9e1f3e71f276a0a51b5de0c507f26b8ffb2bdbf
Markus Ankenbrand <markus@ankenbrand.me>
2016-04-26 13:33:32 +0200

    Add documentation for getBestName function

--- src/webroot/js/helpers/organismDetails.js
+++ src/webroot/js/helpers/organismDetails.js
@@ -1,11 +1,18 @@
-getBestName = function(result){
+/**
+ * Selects the best vernacularName from the object returned by the eol pages API.
+ * It only considers english names (language: en) and preferes those with eol_preferred: true.
+ * The scientificName is used as fallback.
+ * @param eolObject {Object} object returned by the eol pages API
+ * @returns {String} bestName
+ */
+getBestName = function(eolObject){
     var bestName = "";
-    if(typeof result["scientificName"] !== "undefined"){
-        bestName = result["scientificName"];
+    if(typeof eolObject["scientificName"] !== "undefined"){
+        bestName = eolObject["scientificName"];
     }
-    if(typeof result["vernacularNames"] !== "undefined" && result["vernacularNames"].length > 0){
+    if(typeof eolObject["vernacularNames"] !== "undefined" && eolObject["vernacularNames"].length > 0){
         var preferred = false;
-        result["vernacularNames"].forEach(function(value){
+        eolObject["vernacularNames"].forEach(function(value){
             if(value.language === "en"){
                 if(typeof value["eol_preferred"] !== "undefined" && value["eol_preferred"]){
                     preferred = true;
