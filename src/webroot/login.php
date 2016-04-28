a38a909b21ba6d2ea0a83d3214bef26c878256e5
Markus Ankenbrand <markus@ankenbrand.me>
2016-04-28 12:17:52 +0200

    Only try to login user if they are not already logged in

--- src/webroot/login.php
+++ src/webroot/login.php
@@ -13,29 +13,30 @@ if (!isset($_SESSION)){
     session_start();
 }
 
-if (!isset($_GET['code'])) {
+if(!isset($_SESSION['user'])){
+    if (!isset($_GET['code'])) {
 
-    // If we don't have an authorization code then get one
-    $authUrl = $provider->getAuthorizationUrl();
-    $_SESSION['oauth2state'] = $provider->getState();
-    header('Location: '.$authUrl);
-    exit;
+        // If we don't have an authorization code then get one
+        $authUrl = $provider->getAuthorizationUrl();
+        $_SESSION['oauth2state'] = $provider->getState();
+        header('Location: '.$authUrl);
+        exit;
 
-// Check given state against previously stored one to mitigate CSRF attack
-} elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
+    // Check given state against previously stored one to mitigate CSRF attack
+    } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
 
-    unset($_SESSION['oauth2state']);
-    exit('Invalid state');
+        unset($_SESSION['oauth2state']);
+        exit('Invalid state');
 
-} else {
+    } else {
 
-    // Try to get an access token (using the authorization code grant)
-    $token = $provider->getAccessToken('authorization_code', [
-        'code' => $_GET['code']
-    ]);
+        // Try to get an access token (using the authorization code grant)
+        $token = $provider->getAccessToken('authorization_code', [
+            'code' => $_GET['code']
+        ]);
 
-    // Optional: Now you have a token you can look up a users profile data
-    try {
+        // Optional: Now you have a token you can look up a users profile data
+        try {
 
             // We got an access token, let's now get the user's details
             $user = $provider->getResourceOwner($token);
@@ -43,12 +44,13 @@ if (!isset($_GET['code'])) {
 
             $_SESSION['user'] = $user;
 
-    } catch (Exception $e) {
+            // Use these details to create a new profile
+            printf('Hello %s!', $user->getNickname());
 
-        // Failed to get user details
-        exit('Oh dear...');
+        } catch (Exception $e) {
+
+            // Failed to get user details
+            exit('Oh dear...');
+        }
     }
-
-    // Use this to interact with an API on the users behalf
-    echo $token->getToken();
 }
