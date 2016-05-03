803f6cf5c9859a5cdc7ab012c8b19284c05354b7
Markus Ankenbrand <markus@ankenbrand.me>
2016-05-03 08:39:33 +0200

    Add validate function for project upload

--- src/webservice/fennecweb/ajax/upload/Project.php
+++ src/webservice/fennecweb/ajax/upload/Project.php
@@ -20,6 +20,7 @@ class Project extends \fennecweb\WebService
         $db = $this->openDbConnection($querydata);
         $files = array();
         for ($i=0; $i<sizeof($_FILES['files']['tmp_names']); $i++) {
+            $valid = $this->validateFile($_FILES['files']['tmp_names'][$i]);
             $file = array(
                 "name" => $_FILES['files']['names'][$i],
                 "size" => $_FILES['files']['sizes'][$i],
@@ -28,5 +29,17 @@ class Project extends \fennecweb\WebService
             $files[] = $file;
         }
         return array("files" => $files);
+    }
+
+    /**
+     * Function that checks the uploaded file for validity
+     * @param String $filename the uploaded file to check
+     * @returns Either TRUE if the file is valid or a String containing the error message
+     */
+    protected function validateFile($filename)
+    {
+        if (!is_uploaded_file($filename)) {
+            return "Error. There was an error in your request.";
+        }
     }
 }
