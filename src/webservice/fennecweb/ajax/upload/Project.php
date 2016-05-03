5a1cdbd1f90d42b2e6891af81ed6ca6fdfe69436
Markus Ankenbrand <markus@ankenbrand.me>
2016-05-03 07:52:57 +0200

    Update return type of Project upload webservice

--- src/webservice/fennecweb/ajax/upload/Project.php
+++ src/webservice/fennecweb/ajax/upload/Project.php
@@ -18,8 +18,15 @@ class Project extends \fennecweb\WebService
     public function execute($querydata)
     {
         $db = $this->openDbConnection($querydata);
-        $contents = file_get_contents($_FILES['files']['tmp_name'][0]);
-        $data = array("contents" => $contents);
-        return $data;
+        $files = array();
+        for ($i=0; $i<sizeof($_FILES['files']['tmp_names']); $i++) {
+            $file = array(
+                "name" => $_FILES['files']['names'][$i],
+                "size" => $_FILES['files']['sizes'][$i],
+                "error" => null
+            );
+            $files[] = $file;
+        }
+        return array("files" => $files);
     }
 }
