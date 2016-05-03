96b4cd43e4d2f3ce3dc71dcbfebff6b7cec99f81
Markus Ankenbrand <markus@ankenbrand.me>
2016-05-03 07:57:18 +0200

    Add test case for non Json file

--- test/php/fennecweb/ajax/upload/ProjectTest.php
+++ test/php/fennecweb/ajax/upload/ProjectTest.php
@@ -6,6 +6,7 @@ class ProjectTest extends \PHPUnit_Framework_TestCase
 {
     public function testExecute()
     {
+        // Test for error returned by empty file
         $_FILES = array(
             'files' => array(
                 'names' => array('empty'),
@@ -18,6 +19,20 @@ class ProjectTest extends \PHPUnit_Framework_TestCase
         list($service) = WebService::factory('upload/Project');
         $results = ($service->execute(array('dbversion' => DEFAULT_DBVERSION)));
         $expected = array("files"=>array(array("name" => "empty", "size" => 0, "error" => null)));
+        $this->assertEquals($expected, $results);
+        
+        // Test for error returned by non json file
+        $_FILES = array(
+            'files' => array(
+                'names' => array('noJson'),
+                'types' => array('text/plain'),
+                'sizes' => array(71),
+                'tmp_names' => array(__DIR__ . '/testFiles/noJson'),
+                'errors' => 0
+            )
+        );
+        $results = ($service->execute(array('dbversion' => DEFAULT_DBVERSION)));
+        $expected = array("files"=>array(array("name" => "noJson", "size" => 71, "error" => null)));
         $this->assertEquals($expected, $results);
     }
 }
