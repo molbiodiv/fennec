f16ef01fa8888aa58a1d2e2c88eb4dcf6548a5c4
Markus Ankenbrand <markus@ankenbrand.me>
2016-05-04 13:44:55 +0200

    Add updated chado modifications

--- test/php/chado_modifications.sql
+++ test/php/chado_modifications.sql
@@ -49,3 +49,37 @@ CREATE TABLE webuser_data
 WITH (
       OIDS=FALSE
 );
+-- View: full_webuser_data
+
+DROP VIEW IF EXISTS full_webuser_data;
+
+CREATE VIEW full_webuser_data AS
+    SELECT webuser_data.webuser_data_id, webuser_data.project, webuser.oauth_id, oauth_provider.provider, webuser_data.import_date
+    FROM webuser, oauth_provider, webuser_data
+    WHERE webuser.oauth_provider_id = oauth_provider.oauth_provider_id
+    AND webuser_data.webuser_id = webuser.webuser_id;
+
+-- Function: full_webuser_data_insert_row
+
+CREATE OR REPLACE FUNCTION full_webuser_data_insert_row() RETURNS TRIGGER AS $$
+   BEGIN
+      IF (TG_OP = 'INSERT') THEN
+        INSERT INTO oauth_provider (provider) SELECT NEW.provider
+        WHERE NOT EXISTS (
+        SELECT oauth_provider_id FROM oauth_provider WHERE provider = NEW.provider
+        );
+        INSERT INTO webuser (oauth_provider_id, oauth_id) SELECT (SELECT oauth_provider_id FROM oauth_provider WHERE provider = NEW.provider), NEW.oauth_id
+        WHERE NOT EXISTS (
+        SELECT webuser_id FROM webuser WHERE oauth_provider_id = (SELECT oauth_provider_id FROM oauth_provider WHERE provider = NEW.provider) AND oauth_id = NEW.oauth_id
+        );
+        INSERT INTO webuser_data (webuser_id, project) VALUES ((SELECT webuser_id FROM webuser WHERE oauth_provider_id = (SELECT oauth_provider_id FROM oauth_provider WHERE provider = NEW.provider) AND oauth_id = NEW.oauth_id), NEW.project);
+        RETURN NEW;
+      END IF;
+   END;
+$$ LANGUAGE plpgsql;
+-- Trigger: full_webuser_data_insert
+
+DROP TRIGGER IF EXISTS full_webuser_data_insert;
+
+CREATE TRIGGER full_webuser_data_insert INSTEAD OF INSERT ON full_webuser_data FOR EACH ROW EXECUTE PROCEDURE full_webuser_data_insert_row()
+
