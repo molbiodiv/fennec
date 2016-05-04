fddcb129ebf5fda8b5062a5821bf5b9049d621ff
Markus Ankenbrand <markus@ankenbrand.me>
2016-05-04 13:44:34 +0200

    Add api doc with fennecweb subfolder

--- doc/webservice/elementlist.js
+++ doc/webservice/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","fennecweb\\ajax\\details\\Organisms"],["c","fennecweb\\ajax\\details\\OrganismsWithTrait"],["c","fennecweb\\ajax\\details\\Traits"],["c","fennecweb\\ajax\\details\\TraitsOfOrganism"],["c","fennecweb\\ajax\\listing\\Organisms"],["c","fennecweb\\ajax\\listing\\Taxonomy"],["c","fennecweb\\ajax\\listing\\Traits"],["c","fennecweb\\DB"],["c","fennecweb\\WebService"]];
+ApiGen.elements = [["c","fennecweb\\ajax\\details\\Organisms"],["c","fennecweb\\ajax\\details\\OrganismsWithTrait"],["c","fennecweb\\ajax\\details\\Traits"],["c","fennecweb\\ajax\\details\\TraitsOfOrganism"],["c","fennecweb\\ajax\\listing\\Organisms"],["c","fennecweb\\ajax\\listing\\Taxonomy"],["c","fennecweb\\ajax\\listing\\Traits"],["c","fennecweb\\ajax\\upload\\Project"],["c","fennecweb\\DB"],["c","fennecweb\\WebService"]];
