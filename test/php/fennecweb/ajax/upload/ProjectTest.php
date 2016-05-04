d7734ab66b5ddaf258d0800f541e88eb3fc81fab
Markus Ankenbrand <markus@ankenbrand.me>
2016-05-04 09:11:56 +0200

    Add test for successful insertion of json data into webuser_data table

--- test/php/fennecweb/ajax/upload/ProjectTest.php
+++ test/php/fennecweb/ajax/upload/ProjectTest.php
@@ -4,13 +4,17 @@ namespace fennecweb;
 
 class ProjectTest extends \PHPUnit_Framework_TestCase
 {
+    const NICKNAME = 'UploadProjectTestUser';
+    const USERID = 'UploadProjectTestUser';
+    const PROVIDER = 'UploadProjectTest';
+
     public function setUp()
     {
         $_SESSION['user'] = array(
-            'nickname' => $user->getNickName(),
-            'id' => $user->getId(),
-            'provider' => 'github',
-            'token' => $token->getToken()
+            'nickname' => ProjectTest::NICKNAME,
+            'id' => ProjectTest::USERID,
+            'provider' => ProjectTest::PROVIDER,
+            'token' => 'UploadProjectTestUserToken'
         );
     }
 
@@ -74,5 +78,20 @@ class ProjectTest extends \PHPUnit_Framework_TestCase
         $results = ($service->execute(array('dbversion' => DEFAULT_DBVERSION)));
         $expected = array("files"=>array(array("name" => "simpleBiom.json", "size" => 1067, "error" => null)));
         $this->assertEquals($expected, $results);
+        $jsonContent = file_get_contents($_FILES['files']['tmp_names'][0]);
+        $db = DB::getDbForVersion(DEFAULT_DBVERSION);
+        $constant = 'constant';
+        $query_get_project_from_db = <<<EOF
+SELECT project
+    FROM webuser_data WHERE webuser_id =
+        (SELECT webuser_id FROM webuser WHERE oauth_provider_id =
+            (SELECT oauth_provider_id FROM oauth_provider WHERE provider = {$constant('fennecweb\ProjectTest::PROVIDER')})
+            AND oauth_id = {$constant('fennecweb\ProjectTest::USERID')}
+        )
+        AND project::jsonb = '{$jsonContent}'::jsonb
+EOF;
+        $stm_get_project_from_db = $db->prepare($query_get_project_from_db);
+        $stm_get_project_from_db->execute();
+        $this->assertTrue($stm_get_project_from_db->fetch(\PDO::FETCH_ASSOC));
     }
 }
