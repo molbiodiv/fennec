f8f35f761b871c30ed32056677f51a62e83c261d
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-05-18 11:42:31 +0200

    Add docu to execute function of Projects class

--- src/webservice/fennecweb/ajax/listing/Projects.php
+++ src/webservice/fennecweb/ajax/listing/Projects.php
@@ -12,8 +12,16 @@ class Projects extends \fennecweb\WebService
 {
     const ERROR_NOT_LOGGED_IN = "Error. Not logged in.";
     
+    /**
+    * @param $querydata[]
+    * @returns Array $result
+    * <code>
+    * array(array('project_id','import_date','OTUs','sample size'));
+    * </code>
+    */
     public function execute($querydata)
     {
+        
         $result = array();
         return $result;
     }
