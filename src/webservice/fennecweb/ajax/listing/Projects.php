685cd0cf96924984bba7eb6d6a1ba5648a737770
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-05-18 11:31:55 +0200

    Add constant of error message if the user is not logged in to the Projects class

--- src/webservice/fennecweb/ajax/listing/Projects.php
+++ src/webservice/fennecweb/ajax/listing/Projects.php
@@ -6,6 +6,8 @@ use \PDO as PDO;
 
 class Projects extends \fennecweb\WebService
 {
+    const ERROR_NOT_LOGGED_IN = "Error. Not logged in.";
+    
     public function execute($querydata)
     {
         
