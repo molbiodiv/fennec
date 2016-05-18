a5a60cfafc59ef1f92ca2d0f4aadd09bbcb06689
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-05-18 11:37:16 +0200

    Add docu to Projects class

--- src/webservice/fennecweb/ajax/listing/Projects.php
+++ src/webservice/fennecweb/ajax/listing/Projects.php
@@ -4,6 +4,10 @@ namespace fennecweb\ajax\listing;
 
 use \PDO as PDO;
 
+/**
+ * Web Service.
+ * Returns information of all users projects
+ */
 class Projects extends \fennecweb\WebService
 {
     const ERROR_NOT_LOGGED_IN = "Error. Not logged in.";
