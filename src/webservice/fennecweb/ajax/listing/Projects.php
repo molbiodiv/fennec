5dd2785a3be37dfdf1352b62a7759cec085e9140
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-05-18 11:19:24 +0200

    Remove expected array from Projects class and the test fails again

--- src/webservice/fennecweb/ajax/listing/Projects.php
+++ src/webservice/fennecweb/ajax/listing/Projects.php
@@ -8,13 +8,8 @@ class Projects extends \fennecweb\WebService
 {
     public function execute($querydata)
     {
-        $result = array(
-                array("This is a Table ID",
-                "2016-05-17 10:00:52.627236+00",
-                10,
-                5
-                )
-        );
+        
+        $result = array();
         return $result;
     }
 }
