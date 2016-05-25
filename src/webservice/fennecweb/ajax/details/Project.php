f131113a6ce19f866a904613dc98dcf134cec6c3
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-05-25 10:00:25 +0200

    Add empty webservice class and change the error of the failing test

---
+++ src/webservice/fennecweb/ajax/details/Project.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace fennecweb\ajax\details;
+
+use \PDO as PDO;
+
+/**
+ * Web Service.
+ * Returns a project according to the project ID.
+ */
+class Project extends \fennecweb\WebService
+{
+    /**
+    * @param $querydata[]
+    * @returns Array $result
+    * <code>
+    * array('project_id': {biomfile});
+    * </code>
+    */
+    public function execute($querydata)
+    {
+        $db = $this->openDbConnection($querydata);
+        $result = array('data' => array());
+        return $result;
+    }
+}
