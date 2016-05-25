8cf1c7122a5057e2d8cea2d42b0b18b30a8f7142
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-05-25 14:09:34 +0200

    Implement webservice which returns a project according to the projects id

--- src/webservice/fennecweb/ajax/details/Projects.php
+++ src/webservice/fennecweb/ajax/details/Projects.php
@@ -20,7 +20,23 @@ class Projects extends \fennecweb\WebService
     public function execute($querydata)
     {
         $db = $this->openDbConnection($querydata);
-        $result = array('data' => array());
+        $result = array();
+        $project_id = $querydata['id'];
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+        $query_get_project_details = <<<EOF
+SELECT project FROM full_webuser_data WHERE project->>'id' = :project_id AND provider = :provider AND oauth_id = :oauth_id
+EOF;
+        $stm_get_project_details = $db->prepare($query_get_project_details);
+        $stm_get_project_details->bindValue('project_id', $project_id);
+        $stm_get_project_details->bindValue('provider', $_SESSION['user']['provider']);
+        $stm_get_project_details->bindValue('oauth_id', $_SESSION['user']['id']);
+        $stm_get_project_details->execute();
+
+        while ($row = $stm_get_project_details->fetch(PDO::FETCH_ASSOC)) {
+            $result[$project_id] = $row['project'];
+        }
         return $result;
     }
 }
