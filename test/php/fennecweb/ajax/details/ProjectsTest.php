e8969c2800e58a2662264356f323a04ba3724d38
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-05-25 14:13:46 +0200

    Redefine expectation and make the failing test pass

--- test/php/fennecweb/ajax/details/ProjectsTest.php
+++ test/php/fennecweb/ajax/details/ProjectsTest.php
@@ -20,9 +20,9 @@ class ProjectsTest extends \PHPUnit_Framework_TestCase
         list($service) = \fennecweb\WebService::factory('details/Projects');
         $results = ($service->execute(array('dbversion' => DEFAULT_DBVERSION, 'id' => 'table_1')));
         $expected = array(
-                      'table_1' 
-        );
-        $this->assertArraySubset($expected, $results);
+            "table_1" => '{"id": "table_1", "data": [[0, 0, 120.0], [3, 1, 12.0], [5, 2, 20.0], [7, 3, 12.7], [8, 4, 16.0]], "date": "2016-05-03T08:13:41.848780", "rows": [{"id": "OTU_1", "metadata": {}}, {"id": "OTU_2", "metadata": {}}, {"id": "OTU_3", "metadata": {}}, {"id": "OTU_4", "metadata": {}}, {"id": "OTU_5", "metadata": {}}, {"id": "OTU_6", "metadata": {}}, {"id": "OTU_7", "metadata": {}}, {"id": "OTU_8", "metadata": {}}, {"id": "OTU_9", "metadata": {}}, {"id": "OTU_10", "metadata": {}}], "type": "OTU table", "shape": [10, 5], "format": "Biological Observation Matrix 2.1.0", "columns": [{"id": "Sample_1", "metadata": {}}, {"id": "Sample_2", "metadata": {}}, {"id": "Sample_3", "metadata": {}}, {"id": "Sample_4", "metadata": {}}, {"id": "Sample_5", "metadata": {}}], "format_url": "http://biom-format.org", "matrix_type": "sparse", "generated_by": "BIOM-Format 2.1", "matrix_element_type": "float"}'
+        ); 
+        $this->assertEquals($expected, $results);
     }
 }
 
