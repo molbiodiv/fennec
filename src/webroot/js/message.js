6e7288dea89299412f512fc4eb1b71122a8354aa
Markus Ankenbrand <markus@ankenbrand.me>
2016-05-25 13:06:49 +0200

    Add message functionality

---
+++ src/webroot/js/message.js
@@ -0,0 +1,26 @@
+var dialogTemplate = '<div class="alert <%= type %> alert-dismissable" role="alert" style="margin-top: 10px;">';
+dialogTemplate += '<button type="button" class="close" data-dismiss="alert" aria-label="Close">';
+dialogTemplate += '<span aria-hidden="true">&times;</span>';
+dialogTemplate += '</button>';
+dialogTemplate += '<%= message %>';
+dialogTemplate += '</div>';
+dialogTemplate = _.template(dialogTemplate);
+
+/**
+ * This function appends a bootstrap dialog to the message area with the given message and type
+ * @param {type} message - The text that should be shown in the dialog
+ * @param {type} type - The type (color) of the dialog. Possible values: alert-success, alert-warning, alert-danger, alert-info (default)
+ * @returns {void}
+ */
+function showMessageDialog(message, type){
+    var knownTypes = ['alert-success', 'alert-warning', 'alert-danger', 'alert-info'];
+    if(knownTypes.indexOf(type) === -1){
+        var shortTypes = ['success', 'warning', 'danger', 'info'];
+        if(shortTypes.indexOf(type) === -1){
+            type = 'alert-info';
+        } else {
+            type = "alert-" + type;
+        }
+    }
+    $('#global-message-area').append(dialogTemplate({type: type, message: message}));
+}
