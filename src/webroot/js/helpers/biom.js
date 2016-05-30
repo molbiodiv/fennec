72e30ab07fadcbe6100fa5fd43d3bfa4f4ed3593
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-05-30 14:55:43 +0200

    Make all failing tests pass. The biom object is checked by Biom constructor method

--- src/webroot/js/helpers/biom.js
+++ src/webroot/js/helpers/biom.js
@@ -16,9 +16,59 @@ Biom = function(biomObject){
         biom.id = biomObject.id;
     }
     if (biomObject.format === undefined){
-        throw 'There is no name and version of biom format';
+        throw 'There is no name and version of current biom format';
     } else {
-        biom.id = biomObject.id;
+        biom.format = biomObject.format;
+    }
+    if (biomObject.format_url === undefined){
+        throw 'There is no URL providing format details';
+    } else {
+        biom.format_url = biomObject.format_url;
+    }
+    if (biomObject.type === undefined){
+        throw 'There is no table type';
+    } else {
+        biom.type = biomObject.type;
+    }
+    if (biomObject.generated_by === undefined){
+        throw 'There is no package and revision  that built the table';
+    } else {
+        biom.generated_by = biomObject.generated_by;
+    }
+    if (biomObject.date === undefined){
+        throw 'There is no date the table was built';
+    } else {
+        biom.date = biomObject.date;
+    }
+    if (biomObject.rows === undefined){
+        throw 'There are no rows describing the object';
+    } else {
+        biom.rows = biomObject.rows;
+    }
+    if (biomObject.columns === undefined){
+        throw 'There are no columns describing the object';
+    } else {
+        biom.columns = biomObject.columns;
+    }
+    if (biomObject.matrix_type === undefined){
+        throw 'There is no type of matrix data representation';
+    } else {
+        biom.matrix_type = biomObject.matrix_type;
+    }
+    if (biomObject.matrix_element_type === undefined){
+        throw 'There is no value type in matrix';
+    } else {
+        biom.matrix_element_type = biomObject.matrix_element_type;
+    }
+    if (biomObject.shape === undefined){
+        throw 'There is no shape information';
+    } else {
+        biom.shape = biomObject.shape;
+    }
+    if (biomObject.data === undefined){
+        throw 'There is no data';
+    } else {
+        biom.data = biomObject.data;
     }
     return biom;
 };
