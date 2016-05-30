dbc9227952b7c871d9fb1981fcd703f5bd578cef
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-05-30 09:15:37 +0200

    Add js file which contains the biom class. Write docu of constructor

---
+++ src/webroot/js/helpers/biom.js
@@ -0,0 +1,9 @@
+/**
+ * Creates an object of type Biom for showing the projects data using data tables
+ * @constructor
+ * @param {Object} biom - JSON object containing the biom file of a project 
+ * @example 
+ * // initializes a Biom object 
+ * var biom = new Biom(biom)
+ */
+
