c8af747e3a7a4bf4010137e5fb47d8ed5368fc13
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-05-30 14:28:59 +0200

    Change error message

--- src/webroot/js/helpers/biom.js
+++ src/webroot/js/helpers/biom.js
@@ -2,7 +2,7 @@
  * Creates an object of type Biom for showing the projects data using data tables
  * @constructor
  * @param {Object} biomObject - JSON object containing the biom file of a project 
- * @throws Will throw an error if the biomObject does not contain a project id
+ * @throws Will throw an error if the biomObject does not contain a id
  * @example 
  * // initializes a Biom object 
  * var biom = new Biom(biom)
@@ -11,7 +11,12 @@
 Biom = function(biomObject){
     var biom = {};
     if (biomObject.id === undefined){
-        throw 'There is no project id';
+        throw 'There is no id';
+    } else {
+        biom.id = biomObject.id;
+    }
+    if (biomObject.format === undefined){
+        throw 'There is no name and version of biom format';
     } else {
         biom.id = biomObject.id;
     }
