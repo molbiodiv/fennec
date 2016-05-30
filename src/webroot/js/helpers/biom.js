71206238bd32bf8fecf4a606aecac222464f9069
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-05-30 14:21:03 +0200

    Implement id check of biom object

--- src/webroot/js/helpers/biom.js
+++ src/webroot/js/helpers/biom.js
@@ -1,12 +1,19 @@
 /**
  * Creates an object of type Biom for showing the projects data using data tables
  * @constructor
- * @param {Object} biom - JSON object containing the biom file of a project 
+ * @param {Object} biomObject - JSON object containing the biom file of a project 
+ * @throws Will throw an error if the biomObject does not contain a project id
  * @example 
  * // initializes a Biom object 
  * var biom = new Biom(biom)
  */
 
-Biom = function(biom){
+Biom = function(biomObject){
+    var biom = {};
+    if (biomObject.id === undefined){
+        throw 'There is no project id';
+    } else {
+        biom.id = biomObject.id;
+    }
     return biom;
 };
