7a2a8d75ab08b023928fb98f9114dd8e94e5ba53
Markus Ankenbrand <markus@ankenbrand.me>
2016-06-21 15:59:45 +0200

    Fix test case

--- test/php/fennecweb/ajax/edit/AddOrganismIDsToProjectTest.php
+++ test/php/fennecweb/ajax/edit/AddOrganismIDsToProjectTest.php
@@ -46,22 +46,22 @@ class AddOrganismIDsToProjectTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(10, $results['success']);
         list($service) = WebService::factory('details/Projects');
         $results = ($service->execute(array('dbversion' => DEFAULT_DBVERSION, 'ids' => array($id))));
-        $rows = json_decode($results['projects'][$id])['rows'];
-        function ncbi_taxid($row)
+        $rows = json_decode($results['projects'][$id], true)['rows'];
+        $ncbi_taxid = function ($row)
         {
             return($row['metadata']['ncbi_taxid']);
-        }
-        $actual_ncbi_taxids = array_map("ncbi_taxid", $rows);
-        function organism_id($row)
+        };
+        $actual_ncbi_taxids = array_map($ncbi_taxid, $rows);
+        $organism_id = function ($row)
         {
             return($row['metadata']['fennec_organism_id']);
-        }
-        $actual_organism_ids = array_map("organism_id", $rows);
+        };
+        $actual_organism_ids = array_map($organism_id, $rows);
         $expect_ncbi_taxids = array(722820,471708,1671960,634075,1137923,279054,267236,133362,161435,690802);
         $expect_organism_ids = array(16,17,18,21,23,26,27,30,31,32);
         $this->assertEquals($expect_ncbi_taxids, $actual_ncbi_taxids);
         $this->assertEquals($expect_organism_ids, $actual_organism_ids);
-        $this->assertEquals('ncbi_taxid', $rows[3]['fennec_assignment_method']);
-        $this->assertEquals(DEFAULT_DBVERSION, $rows[7]['fennec_dbversion']);
+        $this->assertEquals('ncbi_taxid', $rows[3]['metadata']['fennec_assignment_method']);
+        $this->assertEquals(DEFAULT_DBVERSION, $rows[7]['metadata']['fennec_dbversion']);
     }
 }
