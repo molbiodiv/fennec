4a58000ad36df3a285ca4b02e87a427d21ddaba5
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-06-21 15:16:32 +0200

    Add framework of traits of organisms test

---
+++ test/php/fennecweb/ajax/details/TraitsOfOrganismsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace fennecweb\ajax\details;
+
+use \fennecweb\WebService as WebService;
+
+class TraitsOfOrganismsTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testExecute()
+    {
+
+    }
+}
