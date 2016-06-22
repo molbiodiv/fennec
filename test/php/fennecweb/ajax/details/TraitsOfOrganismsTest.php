63ab05aa216a57b6e6ba7673083c028f818c2f97
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-06-22 10:15:12 +0200

    Add 2 test cases with existing organism_ids. Both tests are still failing

--- test/php/fennecweb/ajax/details/TraitsOfOrganismsTest.php
+++ test/php/fennecweb/ajax/details/TraitsOfOrganismsTest.php
@@ -9,9 +9,16 @@ class TraitsOfOrganismsTest extends \PHPUnit_Framework_TestCase
 
     public function testExecute()
     {
-        //Test if the traits to a selection of organisms is returned correctly
+        //Test if the traits to one organism is returned correctly
         list($service) = WebService::factory('details/TraitsOfOrganisms');
-        $organism_ids = ['13','21','17'];
+        $organism_ids = ['12049'];
+        $results = ($service->execute(array('dbversion' => DEFAULT_DBVERSION, 'organism_ids' => $organism_ids)));
+        $expected = [1 => 'habitat', 5 => 'elevation', 7 => 'plant growth habitat'];
+        $this->assertEquals($expected, $results);
+        
+        //Test if the traits to a collection of organisms is returned correctly
+        list($service) = WebService::factory('details/TraitsOfOrganisms');
+        $organism_ids = ['47757','200', '12049'];
         $results = ($service->execute(array('dbversion' => DEFAULT_DBVERSION, 'organism_ids' => $organism_ids)));
         $expected = [1 => 'habitat', 5 => 'elevation', 7 => 'plant growth habitat'];
         $this->assertEquals($expected, $results);
