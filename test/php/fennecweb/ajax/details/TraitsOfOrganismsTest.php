57d7161e498c93d981d2dd054eb03dbec8c43d71
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-06-22 10:32:12 +0200

    Make the second failing test pass

--- test/php/fennecweb/ajax/details/TraitsOfOrganismsTest.php
+++ test/php/fennecweb/ajax/details/TraitsOfOrganismsTest.php
@@ -22,7 +22,22 @@ class TraitsOfOrganismsTest extends \PHPUnit_Framework_TestCase
         list($service) = WebService::factory('details/TraitsOfOrganisms');
         $organism_ids = ['47757','200', '12049'];
         $results = ($service->execute(array('dbversion' => DEFAULT_DBVERSION, 'organism_ids' => $organism_ids)));
-        $expected = [1 => 'habitat', 5 => 'elevation', 7 => 'plant growth habitat'];
+        $expected = [
+            ['trait_entry_id' => 199, 'cvterm' => 'habitat'],
+            ['trait_entry_id' => 200, 'cvterm' => 'habitat'],
+            ['trait_entry_id' => 201, 'cvterm' => 'habitat'],
+            ['trait_entry_id' => 202, 'cvterm' => 'habitat'],
+            ['trait_entry_id' => 203, 'cvterm' => 'habitat'],
+            ['trait_entry_id' => 387869, 'cvterm' => 'extinction status'],
+            ['trait_entry_id' => 387870, 'cvterm' => 'type specimen repository'],
+            ['trait_entry_id' => 387871, 'cvterm' => 'habitat'],
+            ['trait_entry_id' => 753126, 'cvterm' => 'extinction status'],
+            ['trait_entry_id' => 753127, 'cvterm' => 'geographic distribution'],
+            ['trait_entry_id' => 753128, 'cvterm' => 'geographic distribution'],
+            ['trait_entry_id' => 753129, 'cvterm' => 'type specimen repository'],
+            ['trait_entry_id' => 753130, 'cvterm' => 'habitat'],
+            ['trait_entry_id' => 753131, 'cvterm' => 'geographic distribution']
+        ];
         $this->assertEquals($expected, $results);
     }
 }
