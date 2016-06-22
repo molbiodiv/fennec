bd28dea606a53f3adeab1f74475dfb070812d69c
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-06-22 10:22:22 +0200

    Make one failing test pass

--- test/php/fennecweb/ajax/details/TraitsOfOrganismsTest.php
+++ test/php/fennecweb/ajax/details/TraitsOfOrganismsTest.php
@@ -13,7 +13,9 @@ class TraitsOfOrganismsTest extends \PHPUnit_Framework_TestCase
         list($service) = WebService::factory('details/TraitsOfOrganisms');
         $organism_ids = ['12049'];
         $results = ($service->execute(array('dbversion' => DEFAULT_DBVERSION, 'organism_ids' => $organism_ids)));
-        $expected = [1 => 'habitat', 5 => 'elevation', 7 => 'plant growth habitat'];
+        $expected = [['trait_entry_id' => 753126, 'cvterm' => 'extinction status'], ['trait_entry_id' => 753127, 'cvterm' => 'geographic distribution'],
+            ['trait_entry_id' => 753128, 'cvterm' => 'geographic distribution'],['trait_entry_id' => 753129, 'cvterm' => 'type specimen repository'],
+            ['trait_entry_id' => 753130, 'cvterm' => 'habitat'],['trait_entry_id' => 753131, 'cvterm' => 'geographic distribution']];
         $this->assertEquals($expected, $results);
         
         //Test if the traits to a collection of organisms is returned correctly
