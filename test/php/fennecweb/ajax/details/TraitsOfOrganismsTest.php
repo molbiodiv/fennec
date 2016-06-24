966d567f36d73a5422c1dbdd56d12abd2a6c03b8
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-06-24 08:14:25 +0200

    Make second failing test pass

--- test/php/fennecweb/ajax/details/TraitsOfOrganismsTest.php
+++ test/php/fennecweb/ajax/details/TraitsOfOrganismsTest.php
@@ -42,20 +42,31 @@ class TraitsOfOrganismsTest extends \PHPUnit_Framework_TestCase
         $organism_ids = ['47757','200', '12049'];
         $results = ($service->execute(array('dbversion' => DEFAULT_DBVERSION, 'organism_ids' => $organism_ids)));
         $expected = [
-            ['trait_entry_id' => 199, 'cvterm' => 'habitat'],
-            ['trait_entry_id' => 200, 'cvterm' => 'habitat'],
-            ['trait_entry_id' => 201, 'cvterm' => 'habitat'],
-            ['trait_entry_id' => 202, 'cvterm' => 'habitat'],
-            ['trait_entry_id' => 203, 'cvterm' => 'habitat'],
-            ['trait_entry_id' => 387869, 'cvterm' => 'extinction status'],
-            ['trait_entry_id' => 387870, 'cvterm' => 'type specimen repository'],
-            ['trait_entry_id' => 387871, 'cvterm' => 'habitat'],
-            ['trait_entry_id' => 753126, 'cvterm' => 'extinction status'],
-            ['trait_entry_id' => 753127, 'cvterm' => 'geographic distribution'],
-            ['trait_entry_id' => 753128, 'cvterm' => 'geographic distribution'],
-            ['trait_entry_id' => 753129, 'cvterm' => 'type specimen repository'],
-            ['trait_entry_id' => 753130, 'cvterm' => 'habitat'],
-            ['trait_entry_id' => 753131, 'cvterm' => 'geographic distribution']
+            '29' => [
+                'cvterm' => 'habitat', 
+                'trait_entry_ids' => [199,200,201,202,203],
+                'organism_ids' => [47757]
+            ],
+            '20' => [
+                'cvterm' => 'extinction status', 
+                'trait_entry_ids' => [387869,753126],
+                'organism_ids' => [200,12049]
+            ],
+            '188' => [
+                'cvterm' => 'type specimen repository', 
+                'trait_entry_ids' => [387870,753129],
+                'organism_ids' => [200,12049]
+            ],
+            '27' => [
+                'cvterm' => 'habitat', 
+                'trait_entry_ids' => [387871,753130],
+                'organism_ids' => [200,12049]
+            ],
+            '10' => [
+                'cvterm' => 'geographic distribution', 
+                'trait_entry_ids' => [753127,753128,753131],
+                'organism_ids' => [12049]
+            ]
         ];
         $this->assertEquals($expected, $results);
     }
