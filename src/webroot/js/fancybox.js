05d3ddce7bf149333c2c0dbb5f77c9c9e901fe6d
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2016-07-08 11:33:38 +0200

    Remove js function for displaying the fancybox

--- src/webroot/js/fancybox.js
+++
@@ -1,9 +0,0 @@
-/**
- * Call the fancybox for displaying organism details in a lightbox
- */
-    $(".fancybox").fancybox({
-        minWidth: 1200,
-        maxWidth: 1200,
-        maxHeight: 800,
-        minHeight: 800
-    });
