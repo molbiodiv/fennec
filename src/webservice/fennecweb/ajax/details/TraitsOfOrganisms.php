0c7177471436948c132a286b9d33d54b4fd57c57
Markus Ankenbrand <markus@ankenbrand.me>
2016-07-08 15:26:43 +0200

    Rewrite SQL for organism trait for better performance

--- src/webservice/fennecweb/ajax/details/TraitsOfOrganisms.php
+++ src/webservice/fennecweb/ajax/details/TraitsOfOrganisms.php
@@ -28,8 +28,10 @@ class TraitsOfOrganisms extends \fennecweb\WebService
         $organism_ids = $querydata['organism_ids'];
         $placeholders = implode(',', array_fill(0, count($organism_ids), '?'));
         $query_get_traits_to_organisms = <<<EOF
-SELECT *
-    FROM trait_entry WHERE organism_id IN ($placeholders)
+SELECT trait_entry.*, trait_cvterm.name AS cvtermname, trait_cvterm.definition AS cvtermdefinition
+    FROM trait_entry, trait_cvterm
+    WHERE trait_entry.type_cvterm_id = trait_cvterm.trait_cvterm_id 
+    AND organism_id IN ($placeholders)
 EOF;
         $stm_get_traits_to_organisms = $this->db->prepare($query_get_traits_to_organisms);
         $stm_get_traits_to_organisms->execute($organism_ids);
@@ -37,7 +39,7 @@ EOF;
         $result = array();
         while ($row = $stm_get_traits_to_organisms->fetch(PDO::FETCH_ASSOC)) {
             $type_cvterm_id = $row['type_cvterm_id'];
-            $cvterm = $this->getCvterm($type_cvterm_id);
+            $cvterm = ($row['cvtermname'] == null) ? $row['cvtermdefinition'] : $row['cvtermname'];
             $organism_id = $row['organism_id'];
             $trait_entry_id = $row['trait_entry_id'];
             if (!array_key_exists($type_cvterm_id, $result)) {
@@ -54,23 +56,5 @@ EOF;
             }
         }
         return $result;
-    }
-    
-    /**
-     * Function which returns the cvterm accoring to a trait_entry_id
-     * @param $trait_entry_id
-     * @return cvterm to a given trait_entry_id
-     */
-    private function getCvterm($trait_entry_id)
-    {
-        $query_get_cvterm = <<<EOF
-SELECT name, definition FROM trait_cvterm WHERE trait_cvterm_id = :trait_entry_id
-EOF;
-        $stm_get_cvterm = $this->db->prepare($query_get_cvterm);
-        $stm_get_cvterm->bindValue('trait_entry_id', $trait_entry_id);
-        $stm_get_cvterm->execute();
-        $result = $stm_get_cvterm->fetch(PDO::FETCH_ASSOC);
-        
-        return ($result['name'] == null) ? $result['definition'] : $result['name'];
     }
 }
