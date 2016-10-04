722de305ce991edaeb748b4bc97b78073637ce76
Markus Ankenbrand <markus@ankenbrand.me>
2016-10-04 16:51:34 +0200

    Add trait_format to output of TraitOfOrganisms webservice

--- src/webservice/fennecweb/ajax/details/TraitsOfOrganisms.php
+++ src/webservice/fennecweb/ajax/details/TraitsOfOrganisms.php
@@ -28,9 +28,10 @@ class TraitsOfOrganisms extends \fennecweb\WebService
         $organism_ids = $querydata['organism_ids'];
         $placeholders = implode(',', array_fill(0, count($organism_ids), '?'));
         $query_get_categorical_traits = <<<EOF
-SELECT trait_categorical_entry.id, trait_categorical_entry.organism_id, trait_categorical_entry.trait_type_id, trait_type.type
-    FROM trait_categorical_entry, trait_type
+SELECT trait_categorical_entry.id, trait_categorical_entry.organism_id, trait_categorical_entry.trait_type_id, trait_type.type, trait_format.format
+    FROM trait_categorical_entry, trait_type, trait_format
     WHERE trait_categorical_entry.trait_type_id = trait_type.id
+    AND trait_format.id = trait_type.trait_format_id
     AND organism_id IN ($placeholders)
 EOF;
         $stm_get_categorical_traits = $this->db->prepare($query_get_categorical_traits);
@@ -40,11 +41,13 @@ EOF;
         while ($row = $stm_get_categorical_traits->fetch(PDO::FETCH_ASSOC)) {
             $trait_type = $row['type'];
             $type_cvterm_id = $row['trait_type_id'];
+            $trait_format = $row['format'];
             $organism_id = $row['organism_id'];
             $trait_entry_id = $row['id'];
             if (!array_key_exists($type_cvterm_id, $result)) {
                 $result[$type_cvterm_id] = [
                     'trait_type' => $trait_type,
+                    'trait_format' => $trait_format,
                     'trait_entry_ids' => [$trait_entry_id],
                     'organism_ids' => [$organism_id]
                 ];
