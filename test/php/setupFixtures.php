e740fc41c0255cedab7cb70c64956bada66e3494
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2016-10-04 13:56:02 +0200

    Add userData.sql to setupFixtures

--- test/php/setupFixtures.php
+++ test/php/setupFixtures.php
@@ -23,7 +23,7 @@ echo exec('PGPASSWORD='.$database['DB_PASSWORD'].
         ' -p '.$database['DB_PORT'].
         ' -d '.$database['DB_DBNAME'].'\'');
 echo exec('PGPASSWORD='.$database['DB_PASSWORD'].
-        ' bash -c \'cat '.__DIR__.'/chado_modifications.sql '.__DIR__.'/userData.sql | psql -U '.
+        ' bash -c \'cat '.__DIR__.'/userData.sql | psql -U '.
         $database['DB_USERNAME'].
         ' -h '.$database['DB_HOST'].
         ' -p '.$database['DB_PORT'].
