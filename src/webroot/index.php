f0a67d42b7c8b50bdae8ec4c361d1fa04f664ac3
Markus Ankenbrand <markus@ankenbrand.me>
2016-10-05 14:10:50 +0200

    Fix wrong accessions in index.php an d htaccess

--- src/webroot/index.php
+++ src/webroot/index.php
@@ -114,7 +114,7 @@ switch ($page) {
         $smarty->display('traitSearch.tpl');
         die();
     case 'trait-byid':
-        if (displayTraitsById(requestVal('type_cvterm_id', '/^[0-9]+$/', '')))
+        if (displayTraitsById(requestVal('trait_type_id', '/^[0-9]+$/', '')))
             die();
         break;
     case 'community':
@@ -178,10 +178,10 @@ function displayOrganismSearchResults($searchTerm){
     return true;
 }
 
-function displayTraitsById($type_cvterm_id){
+function displayTraitsById($trait_type_id){
     global $smarty;
     $smarty->assign('type', 'trait');
-    $smarty->assign('type_cvterm_id', $type_cvterm_id);
+    $smarty->assign('trait_type_id', $trait_type_id);
     $smarty->display('traitDetails.tpl');
     return true;
 }
