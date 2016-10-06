5e3610e7c57ef78d16c43370ae7eaad6e3316611
Markus Ankenbrand <markus@ankenbrand.me>
2016-10-06 13:51:39 +0200

    Adjust webservice to restrict trait details to specific organisms

--- src/webservice/fennecweb/ajax/details/Traits.php
+++ src/webservice/fennecweb/ajax/details/Traits.php
@@ -21,28 +21,41 @@ class Traits extends \fennecweb\WebService
     {
         $this->db = $this->openDbConnection($querydata);
         $trait_type_id = $querydata['trait_type_id'];
+        $organism_ids = null;
+        if (isset($querydata['organism_ids']) and is_array($querydata['organism_ids'])){
+            $organism_ids = $querydata['organism_ids'];
+        }
         $result = $this->get_info($trait_type_id);
-        $result['values'] = $this->get_values($trait_type_id);
-        $result['number_of_organisms'] = $this->get_number_of_organisms($trait_type_id);
+        $result['values'] = $this->get_values($trait_type_id, $organism_ids);
+        $result['number_of_organisms'] = $this->get_number_of_organisms($trait_type_id, $organism_ids);
 
         return $result;
     }
 
     /**
      * @param $trait_type_id
+     * @param $organism_ids
      * @return array values of specific trait
      */
-    private function get_values($trait_type_id){
+    private function get_values($trait_type_id, $organism_ids){
+        if ($organism_ids !== null and count($organism_ids) === 0){
+            return array();
+        }
+        $organism_constraint = $this->get_organism_constraint($organism_ids);
         $query_get_values = <<<EOF
 SELECT value, count(value)
     FROM trait_categorical_entry, trait_categorical_value
     WHERE trait_categorical_value_id=trait_categorical_value.id
-    AND trait_categorical_entry.trait_type_id = :trait_type_id
+    AND trait_categorical_entry.trait_type_id = ?
+    {$organism_constraint}
     GROUP BY value;
 EOF;
         $stm_get_values= $this->db->prepare($query_get_values);
-        $stm_get_values->bindValue('trait_type_id', $trait_type_id);
-        $stm_get_values->execute();
+        if($organism_ids !== null){
+            $stm_get_values->execute(array_merge(array($trait_type_id), $organism_ids));
+        } else {
+            $stm_get_values->execute(array($trait_type_id));
+        }
 
         $values = array();
         while ($row = $stm_get_values->fetch(PDO::FETCH_ASSOC)) {
@@ -74,19 +87,37 @@ EOF;
 
     /**
      * @param $trait_type_id
+     * @param $organism_ids
      * @return integer number of organisms which have this trait
      */
-    private function get_number_of_organisms($trait_type_id){
+    private function get_number_of_organisms($trait_type_id, $organism_ids){
+        if ($organism_ids !== null and count($organism_ids) === 0){
+            return 0;
+        }
+        $organism_constraint = $this->get_organism_constraint($organism_ids);
         $query_get_number_of_organisms = <<<EOF
-SELECT count(DISTINCT organism_id) FROM trait_categorical_entry WHERE trait_type_id = :trait_type_id ;
+SELECT count(DISTINCT organism_id) FROM trait_categorical_entry WHERE trait_type_id = ?
+    {$organism_constraint}
 EOF;
         $stm_get_number_of_organisms= $this->db->prepare($query_get_number_of_organisms);
-        $stm_get_number_of_organisms->bindValue('trait_type_id', $trait_type_id);
-        $stm_get_number_of_organisms->execute();
+        if($organism_ids !== null){
+            $stm_get_number_of_organisms->execute(array_merge(array($trait_type_id), $organism_ids));
+        } else {
+            $stm_get_number_of_organisms->execute(array($trait_type_id));
+        }
 
         $row = $stm_get_number_of_organisms->fetch(PDO::FETCH_ASSOC);
         $number_of_organisms = $row['count'];
 
         return $number_of_organisms;
+    }
+
+    private function get_organism_constraint($organism_ids){
+        $organism_constraint = '';
+        if($organism_ids !== null){
+            $placeholders = implode(',', array_fill(0, count($organism_ids), '?'));
+            $organism_constraint = "AND organism_id IN (".$placeholders.") ";
+        }
+        return $organism_constraint;
     }
 }
