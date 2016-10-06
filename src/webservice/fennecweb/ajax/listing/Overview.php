f481f4d50e6a412b5147eb4a7763f5ccef15f51c
Markus Ankenbrand <markus@ankenbrand.me>
2016-10-06 08:22:04 +0200

    Refactor

--- src/webservice/fennecweb/ajax/listing/Overview.php
+++ src/webservice/fennecweb/ajax/listing/Overview.php
@@ -29,22 +29,28 @@ class Overview extends \fennecweb\WebService
         if (!isset($_SESSION)) {
             session_start();
         }
+        $result['projects'] = $this->get_number_of_projects();
+        return $result;
+    }
+
+    /**
+     * @return int number_of_projects
+     */
+    private function get_number_of_projects(){
         if (!isset($_SESSION['user'])) {
-            $result['projects'] = 0;
-        } else {
-            $query_get_user_projects = <<<EOF
+            return 0;
+        }
+        $query_get_user_projects = <<<EOF
 SELECT
     COUNT(*)
     FROM full_webuser_data WHERE provider = :provider AND oauth_id = :oauth_id
 EOF;
-            $stm_get_user_projects = $db->prepare($query_get_user_projects);
-            $stm_get_user_projects->bindValue('provider', $_SESSION['user']['provider']);
-            $stm_get_user_projects->bindValue('oauth_id', $_SESSION['user']['id']);
-            $stm_get_user_projects->execute();
-        
-            $row = $stm_get_user_projects->fetch(PDO::FETCH_ASSOC);
-            $result['projects'] = $row['count'];
-        }
-        return $result;
+        $stm_get_user_projects = $db->prepare($query_get_user_projects);
+        $stm_get_user_projects->bindValue('provider', $_SESSION['user']['provider']);
+        $stm_get_user_projects->bindValue('oauth_id', $_SESSION['user']['id']);
+        $stm_get_user_projects->execute();
+
+        $row = $stm_get_user_projects->fetch(PDO::FETCH_ASSOC);
+        return $row['count'];
     }
 }
