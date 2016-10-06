78849bd0b12f2528fce1ed7016e585ef4dbddbad
Markus Ankenbrand <markus@ankenbrand.me>
2016-10-06 08:11:19 +0200

    Add user data for listing overview test case

--- test/php/userData.sql
+++ test/php/userData.sql
@@ -122,3 +122,27 @@ INSERT INTO full_webuser_data (project,oauth_id,provider,import_date) VALUES ('{
     ]
 }','detailsProjectsTestUser','detailsProjectsTestUser','2016-05-17 10:00:52.627236+00');
 
+-- project for listingOverviewTestUser
+INSERT INTO full_webuser_data (project,oauth_id,provider,import_date) VALUES ('{
+    "id": "table",
+    "format": "Biological Observation Matrix 2.1.0",
+    "format_url": "http://biom-format.org",
+    "matrix_type": "sparse",
+    "generated_by": "BIOM-Format 2.1",
+    "date": "2016-10-06T08:07:40.838383",
+    "type": "OTU table",
+    "matrix_element_type": "float",
+    "shape": [5, 2],
+    "data": [[0,0,120.0],[3,1,12.0],[5,2,20.0],[7,3,12.7],[8,4,16.0]],
+    "rows": [
+        {"id": "OTU_1", "metadata": {}},
+        {"id": "OTU_2", "metadata": {}},
+        {"id": "OTU_3", "metadata": {}},
+        {"id": "OTU_4", "metadata": {}},
+        {"id": "OTU_5", "metadata": {}}
+    ],
+    "columns": [
+        {"id": "Sample_1", "metadata": {}},
+        {"id": "Sample_2", "metadata": {}}
+    ]
+}','listingOverviewTestUser','listingOverviewTestUser','2016-10-06 08:07:40.838383+00');
