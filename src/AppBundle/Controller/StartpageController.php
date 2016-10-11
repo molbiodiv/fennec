65b75999193f3c4644184c1a9e543f48a1ceee44
Markus Ankenbrand <markus@ankenbrand.me>
2016-10-11 17:20:36 +0200

    Add redirect from index to startpage

--- src/AppBundle/Controller/StartpageController.php
+++ src/AppBundle/Controller/StartpageController.php
@@ -5,18 +5,28 @@ namespace AppBundle\Controller;
 use AppBundle\Controller\API\Listing\OverviewController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 class StartpageController extends Controller
 {
     /**
-     * @Route("/", name="startpage")
+     * @Route("/", name="index")
      */
     public function indexAction(Request $request)
     {
-        $oc = $this->get('app.api.listing.overview');
-        $overview = $oc->execute('1.0', $request->getSession());
+        $default_db = $this->getParameter('default_db');
+        return $this->redirectToRoute('startpage', array('dbversion' => $default_db));
+    }
 
-        return $this->render('startpage/index.html.twig', ['type' => 'startpage', 'overview' => $overview, 'title' => 'Welcome']);
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route("/{dbversion}/startpage", name="startpage")
+     */
+    public function startpageAction(Request $request, $dbversion){
+        $oc = $this->get('app.api.listing.overview');
+        $overview = $oc->execute($dbversion, $request->getSession());
+        return $this->render('startpage/index.html.twig', ['type' => 'startpage', 'overview' => $overview, 'title' => 'Welcome', 'dbversion' => $dbversion]);
     }
 }
