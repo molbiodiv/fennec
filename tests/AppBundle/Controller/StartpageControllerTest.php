04c51749e13c1437c89fe792aa4a6efdfe3a4f87
Markus Ankenbrand <markus@ankenbrand.me>
2016-10-11 17:16:14 +0200

    Add test for redirect from index to dbversion specific startpage

--- tests/AppBundle/Controller/StartpageControllerTest.php
+++ tests/AppBundle/Controller/StartpageControllerTest.php
@@ -12,6 +12,13 @@ class StartpageControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
+        $this->assertTrue(
+            $client->getResponse()->isRedirect('/test/startpage'),
+            'response is a redirect to /test/startpage'
+        );
+
+        $crawler = $client->followRedirect();
+
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Projects', $crawler->filter('div')->text());
         $this->assertContains('Organisms', $crawler->filter('div')->text());
