e417a02a62bb43988bd2d8b64b717bd23f93fa8e
Markus Ankenbrand <markus@ankenbrand.me>
2016-10-16 14:35:02 +0200

    Add TraitOfProject webservice

---
+++ src/AppBundle/API/Details/TraitOfProject.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\API\Details;
+
+use AppBundle\API\Webservice;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+/**
+ * Web Service.
+ * Return trait details of project
+ */
+class TraitOfProject extends Webservice
+{
+
+    /**
+     * @inheritdoc
+     * @returns Array $result
+     * see output of details/Traits.php
+     */
+    public function execute(ParameterBag $query, SessionInterface $session = null)
+    {
+        $service = $this->factory('details', 'OrganismsOfProject');
+        $results = $service->execute($query, $session);
+        if (key_exists('error', $results)){
+            return $results;
+        }
+        $service = $this->factory('details', 'traits');
+        $query->set('organism_ids', $results);
+        $results = $service->execute($query, $session);
+
+        return $results;
+    }
+}
