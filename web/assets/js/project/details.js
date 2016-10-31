f3d38e8706f72d55144daedd7f06793edbd04b3b
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2016-10-31 14:22:27 +0100

    Add compiled details.js

---
+++ web/assets/js/project/details.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$('document').ready(function () {
+    var table = React.createElement(
+        'h1',
+        null,
+        'Test'
+    );
+    ReactDOM.render(table, document.getElementById('react-table-test'));
+});
