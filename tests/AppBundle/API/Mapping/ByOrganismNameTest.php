8fd4610894226275bfccd4db7803c46866aec110
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2016-11-03 16:52:57 +0100

    Add test for mapping byOrganismName

---
+++ tests/AppBundle/API/Mapping/ByOrganismNameTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\AppBundle\API\Mapping;
+
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Tests\AppBundle\API\WebserviceTestCase;
+
+class ByOrganismNameTest extends WebserviceTestCase
+{
+    public function testExecute()
+    {
+        $service = $this->webservice->factory('mapping', 'byOrganismName');
+
+        // Test with existing IDs
+        $names = [
+            'Galactites tomentosa',
+            'Austrolejeunea bidentata',
+            'Melilotus infestus',
+            'Cyclogramma sp. 73',
+            'Willkommia'
+        ];
+        $expected = [
+            'Galactites tomentosa' => 505,
+            'Austrolejeunea bidentata' => 1337,
+            'Melilotus infestus' => 1564,
+            'Cyclogramma sp. 73' => 1559,
+            'Willkommia' => 1727
+        ];
+        $result = $service->execute(new ParameterBag(array('dbversion' => $this->default_db, 'names' => $names)),
+            null);
+        $this->assertEquals($expected, $result);
+
+        // Test with some non-existing IDs
+        $names = [
+            'Galactites tomentosa',
+            'Austrolejeunea bidentata',
+            'Melilotus infestus',
+            'Cyclogramma sp. 73',
+            'Willkommia',
+            'non_existing'
+        ];
+        $expected = [
+            'Galactites tomentosa' => 505,
+            'Austrolejeunea bidentata' => 1337,
+            'Melilotus infestus' => 1564,
+            'Cyclogramma sp. 73' => 1559,
+            'Willkommia' => 1727,
+            'non_existing' => null
+        ];
+        $result = $service->execute(new ParameterBag(array('dbversion' => $this->default_db, 'names' => $names)),
+            null);
+        $this->assertEquals($expected, $result);
+    }
+}
