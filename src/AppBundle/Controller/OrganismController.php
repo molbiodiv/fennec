cb516201380bfe1e37371b2887460f7a2f71dfd7
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2016-11-21 15:38:22 +0100

    Rename organism_id to fennec_id in organism controller

--- src/AppBundle/Controller/OrganismController.php
+++ src/AppBundle/Controller/OrganismController.php
@@ -52,22 +52,22 @@ class OrganismController extends Controller
     /**
      * @param Request $request
      * @param $dbversion
-     * @param $organism_id
+     * @param $fennec_id
      * @return Response
-     * @Route("/{dbversion}/organism/details/{organism_id}", name="organism_details", options={"expose" = true})
+     * @Route("/{dbversion}/organism/details/{fennec_id}", name="organism_details", options={"expose" = true})
      */
-    public function detailsAction(Request $request, $dbversion, $organism_id){
+    public function detailsAction(Request $request, $dbversion, $fennec_id){
         $organismDetails = $this->get('app.api.webservice')->factory('details', 'organism');
         $query = $request->query;
         $query->set('dbversion', $dbversion);
-        $query->set('id', $organism_id);
+        $query->set('id', $fennec_id);
         $organismResult = $organismDetails->execute($query, $request->getSession());
         $taxonomy = $this->get('app.api.webservice')->factory('listing', 'taxonomy');
         $taxonomyResult = $taxonomy->execute($query, null);
         $traits = $this->get('app.api.webservice')->factory('details', 'traitsOfOrganisms');
         $traitResult = $traits->execute(new ParameterBag(array(
             'dbversion' => $dbversion,
-            'organism_ids' => array($organism_id)
+            'fennec_ids' => array($fennec_id)
         )), null);
         return $this->render('organism/details.html.twig', [
             'type' => 'organism',
