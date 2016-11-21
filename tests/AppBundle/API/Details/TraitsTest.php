6d3ccc1cae7866c835c004bdfac4885433db7c8f
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2016-11-21 11:14:51 +0100

    Update expectations

--- tests/AppBundle/API/Details/TraitsTest.php
+++ tests/AppBundle/API/Details/TraitsTest.php
@@ -21,46 +21,45 @@ class TraitsTest extends WebserviceTestCase
                 "values" => [
                      "liana" => "136",
                      "nonvascular" => "832",
-                     "forb/herb" => "15090",
+                     "forb/herb" => "15086",
                      "suffrutescent" => "90",
                      "procumbent" => "50",
                      "trailing" => "53",
                      "decumbent" => "50",
                      "epiphyte" => "118",
-                     "tree" => "11199",
+                     "tree" => "11170",
                      "twining" => "23",
                      "climbing plant" => "415",
                      "arborescent" => "3",
-                     "graminoid" => "2648",
-                     "shrub" => "10038",
+                     "graminoid" => "2645",
+                     "shrub" => "10002",
                      "large shrub" => "38",
                      "geophyte" => "41",
                      "vine" => "5560",
-                     "subshrub" => "2442",
+                     "subshrub" => "2440",
                      "woody" => "90",
 
                 ],
                 "trait_type_id" => 1,
-                "name" => "PlantHabit",
-                "ontology_url" => "eol.org/schema/terms/PlantHabit",
+                "name" => "Plant Habit",
+                "ontology_url" => "http://eol.org/schema/terms/PlantHabit",
                 "trait_format" => "categorical_free",
-                "number_of_organisms" => 16425
+                "number_of_organisms" => 16417
             ];
         $this->assertEquals($expected, $results);
 
         $results = $service->execute(
-            new ParameterBag(array('dbversion' => $default_db, 'trait_type_id' => $trait_type_id, 'organism_ids' => [134097, 163840, 24718, 73023, 23057])),
+            new ParameterBag(array('dbversion' => $default_db, 'trait_type_id' => 2, 'organism_ids' => [46032, 6661, 25517, 2888, 109884])),
             null
         );
         $expected = [
                 "values" => [
-                     "forb/herb" => "4",
-                     "shrub" => "2",
-                     "subshrub" => "2"
+                     "annual" => "3",
+                     "perennial" => "3"
                 ],
-                "trait_type_id" => 1,
-                "name" => "PlantHabit",
-                "ontology_url" => "eol.org/schema/terms/PlantHabit",
+                "trait_type_id" => 2,
+                "name" => "Plant Life Cycle Habit",
+                "ontology_url" => "http://purl.obolibrary.org/obo/TO_0002725",
                 "trait_format" => "categorical_free",
                 "number_of_organisms" => 5
             ];
@@ -73,8 +72,8 @@ class TraitsTest extends WebserviceTestCase
         $expected = [
             "values" => [],
             "trait_type_id" => 1,
-            "name" => "PlantHabit",
-            "ontology_url" => "eol.org/schema/terms/PlantHabit",
+            "name" => "Plant Habit",
+            "ontology_url" => "http://eol.org/schema/terms/PlantHabit",
             "trait_format" => "categorical_free",
             "number_of_organisms" => 0
         ];
