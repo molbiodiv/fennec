57be42db9adab35df7d7217e678573be203f4ce1
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2016-11-21 15:04:40 +0100

    Fix webservice

--- src/AppBundle/API/Mapping/ByNcbiTaxid.php
+++ src/AppBundle/API/Mapping/ByNcbiTaxid.php
@@ -23,18 +23,16 @@ class ByNcbiTaxid extends Webservice
         $result = array_fill_keys($ids, null);
         $placeholders = implode(',', array_fill(0, count($ids), '?'));
         $query_get_mapping = <<<EOF
-SELECT organism.organism_id AS organism_id, dbxref.accession AS ncbi_taxid
-    FROM organism, organism_dbxref, dbxref
-        WHERE organism.organism_id = organism_dbxref.organism_id
-        AND organism_dbxref.dbxref_id = dbxref.dbxref_id
-        AND dbxref.db_id=(SELECT db_id FROM db WHERE name='DB:NCBI_taxonomy')
-        AND accession IN ({$placeholders})
+SELECT fennec_id, identifier AS ncbi_taxid
+    FROM fennec_dbxref
+        WHERE db_id=(SELECT db_id FROM db WHERE name='ncbi_taxonomy')
+        AND identifier IN ({$placeholders})
 EOF;
         $stm_get_mapping = $this->db->prepare($query_get_mapping);
         $stm_get_mapping->execute($ids);
 
         while($row = $stm_get_mapping->fetch(\PDO::FETCH_ASSOC)){
-            $result[$row['ncbi_taxid']] = $row['organism_id'];
+            $result[$row['ncbi_taxid']] = $row['fennec_id'];
         }
 
         return $result;
