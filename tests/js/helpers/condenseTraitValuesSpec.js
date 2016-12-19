4724fa99441cbe7e53a751ea7b3097fa0d3f53ff
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2016-12-19 15:21:31 +0100

    Extend test cases

--- tests/js/helpers/condenseTraitValuesSpec.js
+++ tests/js/helpers/condenseTraitValuesSpec.js
@@ -21,8 +21,29 @@ describe('helpers/condenseTraitValues', () => {
         it('function is available', () => {
             assert.equal(typeof condenseTraitValues, 'function');
         });
-        it('function returns empty array if called with an empty array', () => {
-            assert.deepEqual(condenseTraitValues([]), []);
+        it('function returns empty object if called with an empty object', () => {
+            assert.deepEqual(condenseTraitValues({}), {});
+        });
+        it('function returns trait by organism if already unique (single value)', () => {
+            assert.deepEqual(condenseTraitValues({'1': {
+                'trait_type': 'habitat',
+                'trait_entry_ids': [1, 20, 36, 7],
+                'fennec_ids': [13, 20, 5]
+            }}), {'13': 'habitat', '20': 'habitat', '5': 'habitat'});
+        });
+        it('function returns trait by organism if already unique (multiple values)', () => {
+            assert.deepEqual(condenseTraitValues({'1': {
+                'trait_type': 'habitat',
+                'trait_entry_ids': [1, 20, 36, 7],
+                'fennec_ids': [13, 20, 5]
+            }}), {'13': 'habitat', '20': 'habitat', '5': 'habitat'});
+        });
+        it('function returns trait by organism if not unique (multiple values)', () => {
+            assert.deepEqual(condenseTraitValues({'1': {
+                'trait_type': 'habitat',
+                'trait_entry_ids': [1, 20, 36, 7],
+                'fennec_ids': [13, 20, 5]
+            }}), {'13': 'habitat', '20': 'habitat', '5': 'habitat'});
         });
     });
 });
