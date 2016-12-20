c437ee40d1d3ae3d9636083b641d3297e8c2a43c
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2016-12-20 10:58:46 +0100

    Implement condenseTraitValues function

--- web/assets/js/helpers.js
+++ web/assets/js/helpers.js
@@ -1,3 +1,62 @@
+'use strict';
+
+function condenseTraitValues(organismsByValue) {
+    var valueByOrganism = {};
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = Object.keys(organismsByValue).sort()[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var key = _step.value;
+
+            var value = organismsByValue[key];
+            var _iteratorNormalCompletion2 = true;
+            var _didIteratorError2 = false;
+            var _iteratorError2 = undefined;
+
+            try {
+                for (var _iterator2 = value[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+                    var organism = _step2.value;
+
+                    if (organism in valueByOrganism) {
+                        valueByOrganism[organism] += '/' + key;
+                    } else {
+                        valueByOrganism[organism] = key;
+                    }
+                }
+            } catch (err) {
+                _didIteratorError2 = true;
+                _iteratorError2 = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion2 && _iterator2.return) {
+                        _iterator2.return();
+                    }
+                } finally {
+                    if (_didIteratorError2) {
+                        throw _iteratorError2;
+                    }
+                }
+            }
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
+
+    return valueByOrganism;
+}
 "use strict";
 
 /**
