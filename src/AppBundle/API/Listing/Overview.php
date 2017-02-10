049caab48fda3a76c2fdf72963f5eb50c689d319
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2017-02-10 13:26:10 +0100

    Transform listing/overview to orm webservice and call it accordingly

--- src/AppBundle/API/Listing/Overview.php
+++ src/AppBundle/API/Listing/Overview.php
@@ -2,20 +2,23 @@
 
 namespace AppBundle\API\Listing;
 
-use AppBundle\API\Webservice;
+use AppBundle\API\ORMWebservice;
 use AppBundle\User\FennecUser;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpFoundation\ParameterBag;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
-class Overview extends Webservice
+class Overview extends ORMWebservice
 {
-    private $database;
+    /**
+     * @var EntityManager
+     */
+    private $manager;
 
     /**
      * @inheritdoc
      */
     public function execute(ParameterBag $query, FennecUser $user = null){
-        $this->database = $this->getDbFromQuery($query);
+        $this->manager = $this->getManagerFromQuery($query);
         $result = array();
         $result['projects'] = $this->get_number_of_projects($user);
         $result['organisms'] = $this->get_number_of_organisms();
@@ -37,7 +40,7 @@ SELECT
     COUNT(*)
     FROM full_webuser_data WHERE provider = :provider AND oauth_id = :oauth_id
 EOF;
-        $stm_get_user_projects = $this->database->prepare($query_get_user_projects);
+        $stm_get_user_projects = $this->manager->getConnection()->prepare($query_get_user_projects);
         $stm_get_user_projects->bindValue('provider', $user->getProvider());
         $stm_get_user_projects->bindValue('oauth_id', $user->getId());
         $stm_get_user_projects->execute();
@@ -51,7 +54,7 @@ SELECT
     COUNT(*)
     FROM organism
 EOF;
-        $stm_get_number_of_organisms = $this->database->prepare($query_get_number_of_organisms);
+        $stm_get_number_of_organisms = $this->manager->getConnection()->prepare($query_get_number_of_organisms);
         $stm_get_number_of_organisms->execute();
         $row = $stm_get_number_of_organisms->fetch(\PDO::FETCH_ASSOC);
         return $row['count'];
@@ -64,7 +67,7 @@ SELECT
     FROM trait_categorical_entry
     WHERE deletion_date IS NULL
 EOF;
-        $stm_get_number_of_trait_entries = $this->database->prepare($query_get_number_of_trait_entries);
+        $stm_get_number_of_trait_entries = $this->manager->getConnection()->prepare($query_get_number_of_trait_entries);
         $stm_get_number_of_trait_entries->execute();
         $row = $stm_get_number_of_trait_entries->fetch(\PDO::FETCH_ASSOC);
         return $row['count'];
@@ -76,7 +79,7 @@ SELECT
     COUNT(*)
     FROM trait_type
 EOF;
-        $stm_get_number_of_trait_types = $this->database->prepare($query_get_number_of_trait_types);
+        $stm_get_number_of_trait_types = $this->manager->getConnection()->prepare($query_get_number_of_trait_types);
         $stm_get_number_of_trait_types->execute();
         $row = $stm_get_number_of_trait_types->fetch(\PDO::FETCH_ASSOC);
         return $row['count'];
