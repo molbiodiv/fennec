b464fc54061d6ea606a2efd012ba6b921f7fa4d5
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2017-02-14 13:54:47 +0100

    Add shivas versioning bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
+            new Shivas\VersioningBundle\ShivasVersioningBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
