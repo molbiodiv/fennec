66796da4b288dcfd35458eb794115d48164b8b92
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2017-02-28 14:33:23 +0100

    Extend unit test for trait importer

--- tests/AppBundle/Command/ImportTraitValuesCommandTest.php
+++ tests/AppBundle/Command/ImportTraitValuesCommandTest.php
@@ -4,6 +4,7 @@ namespace Tests\AppBundle\Command;
 
 
 use AppBundle\Command\ImportTraitValuesCommand;
+use AppBundle\Entity\TraitCategoricalEntry;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -31,6 +32,9 @@ class ImportTraitValuesCommandTest extends KernelTestCase
         $this->assertNull($em->getRepository('AppBundle:TraitCategoricalValue')->findOneBy(array(
             'value' => 'rainbow'
         )), 'before import there is no flower color "rainbow"');
+        $this->assertNull($em->getRepository('AppBundle:TraitCitation')->findOneBy(array(
+            'citation' => 'fantasy'
+        )), 'before import there is no citation "fantasy"');
         $commandTester->execute(array(
             'command' => $command->getName(),
             '--user-id' => 1,
@@ -41,5 +45,19 @@ class ImportTraitValuesCommandTest extends KernelTestCase
             'value' => 'rainbow'
         ));
         $this->assertNotNull($rainbow, 'after import there is a flower color "rainbow"');
+        $this->assertNotNull($em->getRepository('AppBundle:TraitCitation')->findOneBy(array(
+            'citation' => 'fantasy'
+        )), 'after import there is a citation "fantasy"');
+        $this->assertEquals(4, count($em->getRepository('AppBundle:TraitCategoricalEntry')->findBy(array(
+            'traitCategoricalValue' => $rainbow
+        ))), 'There are four entries with flower color rainbow');
+        /**
+         * @var TraitCategoricalEntry
+         */
+        $privateTrait = $em->getRepository('AppBundle:TraitCategoricalEntry')->findOneBy(array(
+            'traitCategoricalValue' => $rainbow,
+            'fennec_id' => 1111
+        ));
+        $this->assertTrue($privateTrait->isPrivate(), 'One trait is private');
     }
 }
