a2b6e09bbd261e21c6e65ec1dbb1e47563b63510
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2017-03-22 15:42:52 +0100

    Add add-traitformat command

---
+++ src/AppBundle/Command/CreateTraitFormatCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Command;
+
+
+use AppBundle\Entity\TraitFormat;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateTraitFormatCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+        // the name of the command (the part after "bin/console")
+        ->setName('app:create-traitformat')
+
+        // the short description shown while running "php bin/console list"
+        ->setDescription('Creates new TraitFormat.')
+
+        // the full command description shown when running the command with
+        // the "--help" option
+        ->setHelp("This command allows you to create trait types...")
+        ->addArgument('format', InputArgument::REQUIRED, 'The name of the new trait format')
+        ->addOption('connection', 'c', InputOption::VALUE_REQUIRED, 'The database version')
+    ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln([
+            'TraitFormat Creator',
+            '===================',
+            '',
+        ]);
+        $connection_name = $input->getOption('connection');
+        if($connection_name == null) {
+            $connection_name = $this->getContainer()->get('doctrine')->getDefaultConnectionName();
+        }
+        $orm = $this->getContainer()->get('app.orm');
+        $em = $orm->getManagerForVersion($connection_name);
+        $format = $em->getRepository('AppBundle:TraitFormat')->findOneBy(['format' => $input->getArgument('format')]);
+        if($format != null){
+            $output->writeln('<info>TraitFormat already exists, nothing to do.</info>');
+            $output->writeln('<info>TraitFormat ID is: '.$format->getId().'</info>');
+            return;
+        }
+        $traitFormat = new TraitFormat();
+        $traitFormat->setFormat($input->getArgument('format'));
+        $em->persist($traitFormat);
+        $em->flush();
+        $output->writeln('<info>TraitFormat successfully created: '.$traitFormat->getFormat().'</info>');
+        $output->writeln('<info>TraitFormat ID is: '.$traitFormat->getId().'</info>');
+    }
+}
