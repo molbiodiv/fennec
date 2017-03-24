89ee778609929409cc7a2151045e364c46f81584
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-03-24 12:00:08 +0100

    Extract sql query in private function

--- src/AppBundle/API/Listing/Traits.php
+++ src/AppBundle/API/Listing/Traits.php
@@ -31,12 +31,28 @@ class Traits extends Webservice
         if ($query->has('search')) {
             $search = "%".$query->get('search')."%";
         }
+        $data = $this->get_categorical_traits($db, $search, $limit, "categorical");
+        return $data;
+    }
+
+    /**
+     * @param $db
+     * @param $search
+     * @param $limit
+     * @param $trait_format
+     * @return array
+     */
+    private function get_categorical_traits($db, $search, $limit, $trait_format)
+    {
         $query_get_traits = <<<EOF
 SELECT trait_type.type AS name, trait_type_id, count
     FROM 
         (
             SELECT trait_type_id, count(trait_type_id) AS count
-                FROM trait_categorical_entry WHERE deletion_date IS NULL GROUP BY trait_type_id
+EOF;
+        $query_get_traits .= " FROM trait_".$trait_format."_entry WHERE deletion_date IS NULL GROUP BY trait_type_id";
+
+        $query_get_traits .= <<<EOF
         ) AS trait_entry, trait_type
             WHERE trait_type.id=trait_type_id AND trait_type.type ILIKE :search ORDER BY count DESC LIMIT :limit
 EOF;
@@ -44,7 +60,7 @@ EOF;
         $stm_get_traits->bindValue('search', $search);
         $stm_get_traits->bindValue('limit', $limit);
         $stm_get_traits->execute();
-        
+
         $data = array();
         while ($row = $stm_get_traits->fetch(PDO::FETCH_ASSOC)) {
             $result = array();
