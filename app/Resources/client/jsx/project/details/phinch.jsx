14261339af486034173ee7aad1b4ec7c3b23e8fb
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2017-04-24 14:30:58 +0200

    Open phinch in tab automatically

--- app/Resources/client/jsx/project/details/phinch.jsx
+++ app/Resources/client/jsx/project/details/phinch.jsx
@@ -1,40 +1,44 @@
 /* global db */
 /* global biom */
 /* global phinchPreviewPath */
+function adjustIframeHeight() {
+    setTimeout(() => {
+        $('#inspect-with-phinch-iframe').attr('height', $('#inspect-with-phinch-iframe').contents().height() + 20)
+    }, 100)
+}
+
 $('document').ready(() => {
     // Set action for click on inspect with Phinch
     // db is the browser webstorage
-    $('#inspect-with-phinch-button').click(function () {
-        db.open({
-            server: "BiomData",
-            version: 1,
-            schema: {
-                "biom": {
-                    key: {
-                        keyPath: 'id',
-                        autoIncrement: true
-                    }
+    db.open({
+        server: "BiomData",
+        version: 1,
+        schema: {
+            "biom": {
+                key: {
+                    keyPath: 'id',
+                    autoIncrement: true
                 }
             }
-        }).done(function (server) {
-            var biomToStore = {};
-            biomToStore.name = biom.id;
-            let biomString = biom.toString();
-            biomToStore.size = biomString.length;
-            biomToStore.data = biomString;
-            let d = new Date();
-            biomToStore.date = d.getUTCFullYear() + "-" + (d.getUTCMonth() + 1) + "-" + d.getUTCDate() + "T" + d.getUTCHours() + ":" + d.getUTCMinutes() + ":" + d.getUTCSeconds() + " UTC";
-            server.biom.add(biomToStore).done(function (item) {
-                $('#inspect-with-phinch-iframe').show();
-                $('#inspect-with-phinch-iframe').attr('src', phinchPreviewPath);
-            });
+        }
+    }).done(function (server) {
+        var biomToStore = {};
+        biomToStore.name = biom.id;
+        let biomString = biom.toString();
+        biomToStore.size = biomString.length;
+        biomToStore.data = biomString;
+        let d = new Date();
+        biomToStore.date = d.getUTCFullYear() + "-" + (d.getUTCMonth() + 1) + "-" + d.getUTCDate() + "T" + d.getUTCHours() + ":" + d.getUTCMinutes() + ":" + d.getUTCSeconds() + " UTC";
+        server.biom.add(biomToStore).done(function (item) {
+            $('#inspect-with-phinch-iframe').show();
+            $('#inspect-with-phinch-iframe').attr('src', phinchPreviewPath);
         });
     });
 
     // Adjust size of iframe after loading of Phinch
     $('#inspect-with-phinch-iframe').on("load", function () {
-        setTimeout(function () {
-            $('#inspect-with-phinch-iframe').attr('height', $('#inspect-with-phinch-iframe').contents().height() + 20);
-        }, 1000);
+        setTimeout(adjustIframeHeight, 1000);
     });
+
+    $('#inspect-with-phinch-tab').on('click', adjustIframeHeight)
 });
--- web/assets/js/project/details.js
+++ web/assets/js/project/details.js
@@ -568,42 +568,46 @@ $('document').ready(function () {
 /* global db */
 /* global biom */
 /* global phinchPreviewPath */
+function adjustIframeHeight() {
+    setTimeout(function () {
+        $('#inspect-with-phinch-iframe').attr('height', $('#inspect-with-phinch-iframe').contents().height() + 20);
+    }, 100);
+}
+
 $('document').ready(function () {
     // Set action for click on inspect with Phinch
     // db is the browser webstorage
-    $('#inspect-with-phinch-button').click(function () {
-        db.open({
-            server: "BiomData",
-            version: 1,
-            schema: {
-                "biom": {
-                    key: {
-                        keyPath: 'id',
-                        autoIncrement: true
-                    }
+    db.open({
+        server: "BiomData",
+        version: 1,
+        schema: {
+            "biom": {
+                key: {
+                    keyPath: 'id',
+                    autoIncrement: true
                 }
             }
-        }).done(function (server) {
-            var biomToStore = {};
-            biomToStore.name = biom.id;
-            var biomString = biom.toString();
-            biomToStore.size = biomString.length;
-            biomToStore.data = biomString;
-            var d = new Date();
-            biomToStore.date = d.getUTCFullYear() + "-" + (d.getUTCMonth() + 1) + "-" + d.getUTCDate() + "T" + d.getUTCHours() + ":" + d.getUTCMinutes() + ":" + d.getUTCSeconds() + " UTC";
-            server.biom.add(biomToStore).done(function (item) {
-                $('#inspect-with-phinch-iframe').show();
-                $('#inspect-with-phinch-iframe').attr('src', phinchPreviewPath);
-            });
+        }
+    }).done(function (server) {
+        var biomToStore = {};
+        biomToStore.name = biom.id;
+        var biomString = biom.toString();
+        biomToStore.size = biomString.length;
+        biomToStore.data = biomString;
+        var d = new Date();
+        biomToStore.date = d.getUTCFullYear() + "-" + (d.getUTCMonth() + 1) + "-" + d.getUTCDate() + "T" + d.getUTCHours() + ":" + d.getUTCMinutes() + ":" + d.getUTCSeconds() + " UTC";
+        server.biom.add(biomToStore).done(function (item) {
+            $('#inspect-with-phinch-iframe').show();
+            $('#inspect-with-phinch-iframe').attr('src', phinchPreviewPath);
         });
     });
 
     // Adjust size of iframe after loading of Phinch
     $('#inspect-with-phinch-iframe').on("load", function () {
-        setTimeout(function () {
-            $('#inspect-with-phinch-iframe').attr('height', $('#inspect-with-phinch-iframe').contents().height() + 20);
-        }, 1000);
+        setTimeout(adjustIframeHeight, 1000);
     });
+
+    $('#inspect-with-phinch-tab').on('click', adjustIframeHeight);
 });
 'use strict';
 
