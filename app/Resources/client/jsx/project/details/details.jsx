43e8fad4e4d61fd094963ec223f103055c6cfab2
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-05-02 16:50:51 +0200

    Add function for tab redirection in base.jsx

--- app/Resources/client/jsx/project/details/details.jsx
+++ app/Resources/client/jsx/project/details/details.jsx
@@ -45,14 +45,6 @@ $('document').ready(function () {
 
     $('#metadata-overview-sample').append(getMetadataKeys(biom, 'columns').map((text) => $("<li>").text(text)));
     $('#metadata-overview-observation').append(getMetadataKeys(biom, 'rows').map((text) => $("<li>").text(text)));
-
-    var url = document.location.toString();
-    if (url.match('#')) {
-        $('.nav-tabs a[href="#'+url.split('#')[1]+'"]').tab('show');
-    }
-    $('.nav-tabs a').on('shown.bs.tab', function (e) {
-      window.location.hash = e.target.hash;
-    })
 });
 
 /**
--- web/assets/js/project/details.js
+++ web/assets/js/project/details.js
@@ -51,14 +51,6 @@ $('document').ready(function () {
     $('#metadata-overview-observation').append(getMetadataKeys(biom, 'rows').map(function (text) {
         return $("<li>").text(text);
     }));
-
-    var url = document.location.toString();
-    if (url.match('#')) {
-        $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
-    }
-    $('.nav-tabs a').on('shown.bs.tab', function (e) {
-        window.location.hash = e.target.hash;
-    });
 });
 
 /**
