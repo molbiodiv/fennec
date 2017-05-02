6c8cb7a06897df7fac92f1c15b751add7ec8fb5b
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-05-02 16:06:25 +0200

    Extend sql statement in order to get unit of trait types and change test expectation

--- src/AppBundle/API/Details/TraitsOfOrganisms.php
+++ src/AppBundle/API/Details/TraitsOfOrganisms.php
@@ -35,7 +35,7 @@ class TraitsOfOrganisms extends Webservice
         }
         $placeholders = implode(',', array_fill(0, count($fennec_ids), '?'));
         $query_get_categorical_traits = <<<EOF
-(SELECT trait_categorical_entry.id, trait_categorical_entry.fennec_id, trait_categorical_entry.trait_type_id, trait_type.type, trait_format.format
+(SELECT trait_categorical_entry.id, trait_categorical_entry.fennec_id, trait_categorical_entry.trait_type_id, trait_type.type, trait_format.format, trait_type.unit
     FROM trait_categorical_entry, trait_type, trait_format
     WHERE trait_categorical_entry.trait_type_id = trait_type.id
     AND trait_format.id = trait_type.trait_format_id
@@ -43,7 +43,7 @@ class TraitsOfOrganisms extends Webservice
     AND fennec_id IN ($placeholders))
 UNION
 (SELECT
-    trait_numerical_entry.id, trait_numerical_entry.fennec_id, trait_numerical_entry.trait_type_id, trait_type.type, trait_format.format
+    trait_numerical_entry.id, trait_numerical_entry.fennec_id, trait_numerical_entry.trait_type_id, trait_type.type, trait_format.format, trait_type.unit
     FROM trait_numerical_entry, trait_type, trait_format
     WHERE trait_numerical_entry.trait_type_id = trait_type.id
     AND trait_format.id = trait_type.trait_format_id
--- tests/AppBundle/API/Details/TraitsOfOrganismsTest.php
+++ tests/AppBundle/API/Details/TraitsOfOrganismsTest.php
@@ -27,18 +27,20 @@ class TraitsOfOrganismsTest extends WebserviceTestCase
             '1' => [
                 'trait_type' => 'Plant Habit',
                 'trait_format' => 'categorical_free',
-                'trait_entry_ids' => [54134, 54133],
-                'fennec_ids' => [615]
+                'trait_entry_ids' => [54133, 54134],
+                'fennec_ids' => [615],
+                'unit' => null
             ],
             '2' => [
                 'trait_type' => 'Plant Life Cycle Habit',
                 'trait_format' => 'categorical_free',
-                'trait_entry_ids' => [83437, 83435, 83436],
-                'fennec_ids' => [615]
+                'trait_entry_ids' => [83437, 83436, 83435],
+                'fennec_ids' => [615],
+                'unit' => null
             ]
         ];
         $this->assertEquals($expected, $results);
-        
+
         //Test if the traits to a collection of organisms is returned correctly
         $parameterBag = new ParameterBag(array('dbversion' => $default_db, 'fennec_ids' => array(4207, 5637, 23547, 181840)));
         $results = $traitsOfOrganisms->execute($parameterBag, $user);
@@ -47,25 +49,29 @@ class TraitsOfOrganismsTest extends WebserviceTestCase
                 'trait_type' => 'Plant Habit',
                 'trait_format' => 'categorical_free',
                 'trait_entry_ids' => [50128, 50129, 61731, 61729, 50130, 61728, 61609, 61730],
-                'fennec_ids' => [4207, 23547, 5637]
+                'fennec_ids' => [4207, 23547, 5637],
+                'unit' => null
             ],
             '2' => [
                 'trait_type' => 'Plant Life Cycle Habit',
                 'trait_format' => 'categorical_free',
                 'trait_entry_ids' => [82072, 85589, 85541],
-                'fennec_ids' => [4207, 23547, 5637]
+                'fennec_ids' => [4207, 23547, 5637],
+                'unit' => null
             ],
             '3' => [
                 'trait_type' => 'IUCN Threat Status',
                 'trait_format' => 'categorical_free',
                 'trait_entry_ids' => [88860],
-                'fennec_ids' => [181840]
+                'fennec_ids' => [181840],
+                'unit' => null
             ],
             '4' => [
                 'trait_type' => 'Flower Color',
                 'trait_format' => 'categorical_free',
                 'trait_entry_ids' => [113422],
-                'fennec_ids' => [4207]
+                'fennec_ids' => [4207],
+                'unit' => null
             ]
         ];
         $this->assertEquals($expected, $results);
@@ -78,37 +84,43 @@ class TraitsOfOrganismsTest extends WebserviceTestCase
                 'trait_type' => 'Plant Habit',
                 'trait_format' => 'categorical_free',
                 'trait_entry_ids' => [28957, 28954, 23398, 28956, 28955],
-                'fennec_ids' => [1262, 5514]
+                'fennec_ids' => [1262, 5514],
+                'unit' => null
             ],
             '2' => [
                 'trait_type' => 'Plant Life Cycle Habit',
                 'trait_format' => 'categorical_free',
                 'trait_entry_ids' => [74215, 72164],
-                'fennec_ids' => [1262, 5514]
+                'fennec_ids' => [1262, 5514],
+                'unit' => null
             ],
             '3' => [
                 'trait_type' => 'IUCN Threat Status',
                 'trait_format' => 'categorical_free',
                 'trait_entry_ids' => [91532, 95880, 104870],
-                'fennec_ids' => [1262, 25219, 5514]
+                'fennec_ids' => [1262, 25219, 5514],
+                'unit' => null
             ],
             '4' => [
                 'trait_type' => 'Flower Color',
                 'trait_format' => 'categorical_free',
                 'trait_entry_ids' => [112441],
-                'fennec_ids' => [1262]
+                'fennec_ids' => [1262],
+                'unit' => null
             ],
             '7' => [
                 'trait_type' => 'Leaf size',
                 'trait_format' => 'numerical',
                 'trait_entry_ids' => [7094, 7075, 7090, 7093],
-                'fennec_ids' => [1262, 5514, 25219]
+                'fennec_ids' => [1262, 5514, 25219],
+                'unit' => 'mm^2'
             ],
             '8' => [
                 'trait_type' => 'Leaf mass',
                 'trait_format' => 'numerical',
                 'trait_entry_ids' => [10482, 10483, 10500, 10495, 10501],
-                'fennec_ids' => [5514, 1262, 25219]
+                'fennec_ids' => [5514, 1262, 25219],
+                'unit' => 'mg'
             ]
         ];
         $this->assertEquals($expected, $results);
