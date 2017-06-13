942b48533d01fddffc18101057816f559f1a0fad
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-06-13 16:26:25 +0200

    Add quick search jsx template for organism page

---
+++ app/Resources/client/jsx/organism/search/quickSearch.jsx
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    //autocomplete for organism search
+    $("#search_organism").autocomplete({
+        position: {
+            my: "right top", at: "right bottom"
+        },
+        source: function (request, response) {
+            var search = request.term;
+            $.ajax({
+                url: "{{ path('api', {'namespace': 'listing', 'classname': type~'s'}) }}",
+                data: {term:  request.term, limit: 500, search: search, dbversion: '{{ dbversion }}'},
+                dataType: "json",
+                success: function (data) {
+                    response(data);
+                }
+            });
+        },
+        minLength: 3
+    });
+
+    $("#search_organism").data("ui-autocomplete")._renderItem = function (ul, item) {
+            var details = Routing.generate('{{ type }}_details', {'dbversion': '{{ dbversion }}', 'fennec_id': item.fennec_id});
+            var link = "<a href='"+details+"'><span style='display:inline-block; width: 100%; font-style: italic;'>" + item.scientific_name + "</span></a>";
+        var li = $("<li>")
+                .append(link)
+                .appendTo(ul);
+        return li;
+    };
+
+    $("#btn_search_organism").click(function(){
+        var searchTerm = $("#search_organism").val();
+        var resultPage = Routing.generate('{{ type }}_result', {'dbversion': '{{ dbversion }}', 'limit': 500, 'search': searchTerm});
+        window.location.href = resultPage;
+    });
+});
