95d4b088ae1b4da5cb4a42d26c118a76e27f8d8e
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2017-08-03 15:39:29 +0200

    Remove sass handling from gulp (handled by webpack)

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,6 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sassLint = require('gulp-sass-lint');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
 // testing
 var mocha = require('gulp-mocha');
 
@@ -13,26 +9,7 @@ gulp.task('test', function() {
         .pipe(mocha({reporter: 'spec', useColors: true}))
 });
 
-gulp.task('sass', function () {
-  return gulp.src('app/Resources/client/scss/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(concat('style.css'))
-    .pipe(sourcemaps.write('.', {includeContent: false}))
-    .pipe(gulp.dest('web/assets/css'));
-});
-
-gulp.task('sassLint', function() {
-  gulp.src('web/assets/scss/*.s+(a|c)ss')
-    .pipe(sassLint())
-    .pipe(sassLint.format())
-    .pipe(sassLint.failOnError());
-});
-
-gulp.task('css', ['sassLint','sass'], function () {
-});
-
-gulp.task('default', ['css','test'], function() {
+gulp.task('default', ['test'], function() {
   // place code for your default task here
 });
 
