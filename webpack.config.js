e9f72fc8fead6bbf5192fdf01afd8c71bb6dda9e
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2017-08-03 15:32:40 +0200

    Create shared global js/css via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,12 @@ var Encore = require('@symfony/webpack-encore');
 
 Encore
     // directory where all compiled assets will be stored
-    .setOutputPath('web/assets/js/')
+    .setOutputPath('web/assets/js')
     // what's the public path to this directory (relative to your project's document root dir)
-    .setPublicPath('/js')
+    .setPublicPath('/assets/js')
     // empty the outputPath dir before each build
     .cleanupOutputBeforeBuild()
     // will output as web/build/app.js
-    .addEntry('base', ['babel-polyfill', './app/Resources/client/jsx/base.jsx'])
-    .addEntry('helpers', ['./app/Resources/client/jsx/helpers.jsx'])
     .addEntry('project/details', ['./app/Resources/client/jsx/project/details.jsx'])
     .addEntry('project/helpers', ['./app/Resources/client/jsx/project/helpers.jsx'])
     .addEntry('project/traitDetails', ['./app/Resources/client/jsx/project/traitDetails.jsx'])
@@ -18,6 +16,16 @@ Encore
     .addEntry('organism/search', ['./app/Resources/client/jsx/organism/search.jsx'])
     .addEntry('trait/browse', ['./app/Resources/client/jsx/trait/browse.jsx'])
     .addEntry('trait/search', ['./app/Resources/client/jsx/trait/search.jsx'])
+    .enableSassLoader()
+    .createSharedEntry('global', [
+        'babel-polyfill',
+        'lodash',
+        'jquery',
+        './app/Resources/client/jsx/base.jsx',
+        './app/Resources/client/jsx/helpers.jsx',
+        './app/Resources/client/scss/base.scss'
+    ])
+    .autoProvidejQuery()
     .enableSourceMaps(!Encore.isProduction())
 ;
 
