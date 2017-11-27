88270fe90fe8e49bb2991166e1c43485207143f5
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-11-27 15:33:03 +0100

    Update FennecUser Entity

--- src/AppBundle/User/FennecUser.php
+++ src/AppBundle/User/FennecUser.php
@@ -15,10 +15,6 @@ use FOS\UserBundle\Model\User as BaseUser;
 class FennecUser extends BaseUser
 {
     /**
-     * @var string
-     */
-    protected $username;
-    /**
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      * @ORM\Column(type="integer")
@@ -30,19 +26,6 @@ class FennecUser extends BaseUser
     private $provider;
 
     /**
-     * User constructor.
-     * @param string $id
-     * @param string $username
-     * @param string $provider
-     */
-    public function __construct($id, $username, $provider)
-    {
-        parent::__construct($username);
-        $this->id = $id;
-        $this->provider = $provider;
-    }
-
-    /**
      * @return string
      */
     public function getId()
@@ -50,51 +33,44 @@ class FennecUser extends BaseUser
         return $this->id;
     }
 
-    /**
-     * @return string
-     */
-    public function getProvider()
-    {
-        return $this->provider;
-    }
 
-    /**
-     * @param UserInterface $user
-     * @return bool
-     */
-    public function equals(UserInterface $user)
-    {
-        if(get_class($this) !== get_class($user)){
-            return false;
-        }
-        return $this->id === $user->getId() && $this->username === $user->getUsername() && $this->provider === $user->getProvider();
-    }
-
-    /**
-     * @param $em EntityManager
-     * @param $create boolean
-     * @return Webuser|null
-     */
-    public function getWebuser($em, $create = false){
-        $webuser= null;
-        if($em !== null){
-            $provider = $em->getRepository('AppBundle:OauthProvider')->findOneBy(array('provider' => $this->getProvider()));
-            if($provider === null && $create){
-                $provider = new OauthProvider();
-                $provider->setProvider($this->getProvider());
-                $em->persist($provider);
-            }
-            if($provider !== null){
-                $webuser = $em->getRepository('AppBundle:Webuser')->findOneBy(array('oauthProvider' => $provider, 'oauthId' => $this->getId()));
-                if($webuser === null && $create){
-                    $webuser = new Webuser();
-                    $webuser->setOauthProvider($provider);
-                    $webuser->setOauthId($this->getId());
-                    $em->persist($webuser);
-                    $em->flush();
-                }
-            }
-        }
-        return $webuser;
-    }
+//    /**
+//     * @param UserInterface $user
+//     * @return bool
+//     */
+//    public function equals(UserInterface $user)
+//    {
+//        if(get_class($this) !== get_class($user)){
+//            return false;
+//        }
+//        return $this->id === $user->getId() && $this->username === $user->getUsername() && $this->provider === $user->getProvider();
+//    }
+//
+//    /**
+//     * @param $em EntityManager
+//     * @param $create boolean
+//     * @return Webuser|null
+//     */
+//    public function getWebuser($em, $create = false){
+//        $webuser= null;
+//        if($em !== null){
+//            $provider = $em->getRepository('AppBundle:OauthProvider')->findOneBy(array('provider' => $this->getProvider()));
+//            if($provider === null && $create){
+//                $provider = new OauthProvider();
+//                $provider->setProvider($this->getProvider());
+//                $em->persist($provider);
+//            }
+//            if($provider !== null){
+//                $webuser = $em->getRepository('AppBundle:Webuser')->findOneBy(array('oauthProvider' => $provider, 'oauthId' => $this->getId()));
+//                if($webuser === null && $create){
+//                    $webuser = new Webuser();
+//                    $webuser->setOauthProvider($provider);
+//                    $webuser->setOauthId($this->getId());
+//                    $em->persist($webuser);
+//                    $em->flush();
+//                }
+//            }
+//        }
+//        return $webuser;
+//    }
 }
