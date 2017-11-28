39b8fb3745a2d196cce653c5021809985aa3d24a
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-11-28 13:13:02 +0100

    Add new controller for rendering the dropdown menu

---
+++ src/AppBundle/Controller/DBVersionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DBVersionController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        $params = $request->get('_route_params');
+        if(count($params) != 0){
+            $dbversion = $params['dbversion'];
+        } else {
+            $default_db = $this->getParameter('dbal')['default_connection'];
+            $dbversion = $default_db;
+        }
+        return $this->render('components/dbversionDropdownMenu.html.twig', array('dbversion' => $dbversion));
+    }
+}
