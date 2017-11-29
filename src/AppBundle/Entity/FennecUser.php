0092b0bf3e701aaa9186fdf00f929264baf2f304
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-11-29 10:59:15 +0100

    Add more properties to FennecUser class

--- src/AppBundle/Entity/FennecUser.php
+++ src/AppBundle/Entity/FennecUser.php
@@ -18,6 +18,48 @@ class FennecUser extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $lastName;
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
     public function getId()
     {
         return $this->id;
