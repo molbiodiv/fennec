6ed6ac84529c6e1ee518318ae277322c51a2c938
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-11-29 11:24:20 +0100

    Extend default form with two new properties

--- src/AppBundle/Form/RegistrationFormType.php
+++ src/AppBundle/Form/RegistrationFormType.php
@@ -1,12 +1,22 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: sonja
- * Date: 11/29/17
- * Time: 11:01 AM
- */
-class RegistrationFormType
-{
+namespace AppBundle\Form;
 
+use Symfony\Component\Form\AbstractType;
+use \Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseRegistrationFormType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName');
+    }
+
+    public function getParent()
+    {
+        return BaseRegistrationFormType::class;
+    }
 }
