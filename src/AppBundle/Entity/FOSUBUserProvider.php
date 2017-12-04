ce6ac8a58be741257cb7089ac659dd48d6dbee7b
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-12-04 11:38:43 +0100

    Add connect function and function for loading user data

--- src/AppBundle/Entity/FOSUBUserProvider.php
+++ src/AppBundle/Entity/FOSUBUserProvider.php
@@ -8,8 +8,68 @@
 
 namespace AppBundle\Entity;
 
+use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
+use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider as BaseClass;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-class FOSUBUserProvider
+
+class FOSUBUserProvider extends BaseClass
 {
+    public function connect(UserInterface $user, UserResponseInterface $response)
+    {
+        $property = $this->getProperty($response);
+        $username = $response->getUsername();
 
+        $service = $response->getResourceOwner()->getName();
+
+        $setter = 'set' . ucfirst($service);
+        $setter_id = $setter . 'Id';
+        $setter_token = $setter . 'AccessToken';
+
+        if (null !== $previousUser = $this->userManager->findUserBy(array($property => $username))) {
+            $previousUser->$setter_id(null);
+            $previousUser->$setter_token(null);
+            $this->userManager->updateUser($previousUser);
+        }
+
+        //we connect current user
+        $user->$setter_id($username);
+        $user->$setter_token($response->getAccessToken());
+        $this->userManager->updateUser($user);
+    }
+
+    public function loadUserByOAuthUserResponse(UserResponseInterface $response)
+    {
+        $username = $response->getUsername();
+        $email = $response->getEmail();
+        $property = $this->getProperty($response);
+        $user = $this->userManager->findUserBy(array($property => $username));
+
+        if (null === $user) {
+            $service = $response->getResourceOwner()->getName();
+            $setter = 'set' . ucfirst($service);
+            $setter_id = $setter . 'Id';
+            $setter_token = $setter . 'AccessToken';
+            // create new user here
+            $user = $this->userManager->createUser();
+            $user->$setter_id($username);
+            $user->$setter_token($response->getAccessToken());
+            //I have set all requested data with the user's username
+            //modify here with relevant data
+            $user->setUsername($username);
+            $user->setEmail($email);
+            $user->setPassword($username);
+            $user->setEnabled(true);
+            $this->userManager->updateUser($user);
+            return $user;
+        }
+
+        //if user exists - go with the HWIOAuth way
+        $user = parent::loadUserByOAuthUserResponse($response);
+        $serviceName = $response->getResourceOwner()->getName();
+        $setter = 'set' . ucfirst($serviceName) . 'AccessToken';
+        //update access token
+        $user->$setter($response->getAccessToken());
+        return $user;
+    }
 }
