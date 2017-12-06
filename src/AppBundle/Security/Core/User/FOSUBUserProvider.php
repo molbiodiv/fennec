0347400e699d175b4d2cdc2d48dcdd1c6032814c
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-12-06 11:11:32 +0100

    Define new class FOSUBUserProvider

---
+++ src/AppBundle/Security/Core/User/FOSUBUserProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle\Security\Core\User;
+
+use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
+use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider as BaseFOSUBProvider;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class FOSUBUserProvider extends BaseFOSUBProvider
+{
+
+}
