c1bd1631b0196caa405225608b1231bf7e87d950
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-12-10 09:35:20 +0100

    Rename test class

--- tests/AppBundle/Command/CreateFennecUserCommandTest.php
+++ tests/AppBundle/Command/CreateFennecUserCommandTest.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
 
-class CreateWebuserCommandTest extends KernelTestCase
+class CreateFennecUserCommandTest extends KernelTestCase
 {
     public function testExecute()
     {
