06da49a03d5efbf14de951b4a6187f1908765a05
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-12-10 07:57:19 +0100

    Change structure of overview test

--- tests/AppBundle/API/Listing/OverviewTest.php
+++ tests/AppBundle/API/Listing/OverviewTest.php
@@ -12,7 +12,26 @@ class OverviewTest extends WebserviceTestCase
     const USERID = 'listingOverviewTestUser';
     const PROVIDER = 'listingOverviewTestUser';
 
-    public function testExecute()
+    private $em;
+
+    public function setUp()
+    {
+        $kernel = self::bootKernel();
+
+        $this->em = $kernel->getContainer()
+            ->get('app.orm')
+            ->getManagerForVersion('test');
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        $this->em->close();
+        $this->em = null; // avoid memory leaks
+    }
+
+    public function testOverviewIfUserIsNotLoggedIn()
     {
         //Test for overview if user is not logged in
         $default_db = $this->default_db;
@@ -22,15 +41,22 @@ class OverviewTest extends WebserviceTestCase
         $expected = array(
             "projects" => 0,
             "organisms" => 198102,
-            "trait_entries" => 91494+7074,
+            "trait_entries" => 91494 + 7074,
             "trait_types" => 7
         );
         $this->assertEquals($expected, $results);
+    }
 
+    public function testOverviewIfUserIsLoggedIn(){
+        $default_db = $this->default_db;
         //Test of correct project if the user has only one project
-        $this->user = new FennecUser(OverviewTest::USERID,OverviewTest::NICKNAME,OverviewTest::PROVIDER);
-        $results = $overview->execute($parameterBag, $this->user);
-        $expected['projects'] = 1;
+        $user = $this->em->getRepository('AppBundle:FennecUser')->findOneBy(array(
+            'username' => OverviewTest::NICKNAME
+        ));
+        $overview = $this->webservice->factory('listing', 'overview');
+        $parameterBag = new ParameterBag(array('dbversion' => $default_db));
+        $results = $overview->execute($parameterBag, $user);
+        $expected['projects'] = 0;
         $this->assertEquals($expected, $results);
     }
 }
