605db9e5a6c776eff6a5a821536f0be50f2d59c4
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-12-20 13:51:35 +0100

    Update execute function in organism detaisl service and remove oboslet sql statements

--- src/AppBundle/API/Details/Organism.php
+++ src/AppBundle/API/Details/Organism.php
@@ -25,67 +25,13 @@ class Organism
 
 
     /**
-     * @param $query ParameterBag
+     * @param $fennecId
+     * @param $dbversion DBVersion
      * @param $user FennecUser
      * @returns array of details
      */
-    public function execute(ParameterBag $query, FennecUser $user = null)
+    public function execute(int $fennecId, DBVersion $dbversion, FennecUser $user = null)
     {
-        $this->db = $this->getManagerFromQuery($query)->getConnection();
-        $id = $query->get('id');
-        $placeholders = implode(',', array_fill(0, count($id), '?'));
-        $query_get_organisms = <<<EOF
-SELECT *
-    FROM organism WHERE fennec_id IN ($placeholders)
-EOF;
-        $stm_get_organisms = $this->db->prepare($query_get_organisms);
-        $stm_get_organisms->execute(array($id));
-
-        $result = array();
-        while ($row = $stm_get_organisms->fetch(PDO::FETCH_ASSOC)) {
-            $result['fennec_id'] = $row['fennec_id'];
-            $result['scientific_name'] = $row['scientific_name'];
-            $result['eol_identifier'] = $this->getIdentifierDbxref($result['fennec_id'], 'EOL');
-            $result['ncbi_identifier'] = $this->getIdentifierDbxref($result['fennec_id'], 'ncbi_taxonomy');
-        }
-        return $result;
-    }
-    
-    /**
-     * @param int $fennec_id
-     * @param string $db_name
-     * @return string $identifier identifier of the current organism in the defined database
-     */
-    private function getIdentifierDbxref($fennec_id, $db_name)
-    {
-        $query_get_DB_Id = <<<EOF
-SELECT db_id
-    FROM db WHERE name = ?
-EOF;
-            $stm_get_DB_Id = $this->db->prepare($query_get_DB_Id);
-            $stm_get_DB_Id->execute(array($db_name));
-        while ($row = $stm_get_DB_Id->fetch(PDO::FETCH_ASSOC)) {
-            $db_id = $row['db_id'];
-        }
-        if (!isset($db_id)) {
-            return "";
-        }
-            
-            $query_get_identifier = <<<EOF
-SELECT identifier
-    FROM fennec_dbxref
-    WHERE db_id = :db_id AND fennec_id = :fennec_id;
-EOF;
-            $stm_get_identifier = $this->db->prepare($query_get_identifier);
-            $stm_get_identifier->bindValue('db_id', $db_id);
-            $stm_get_identifier->bindValue('fennec_id', $fennec_id);
-            $stm_get_identifier->execute();
-        while ($row = $stm_get_identifier->fetch(PDO::FETCH_ASSOC)) {
-            $identifier = $row['identifier'];
-        }
-        if (!isset($identifier)) {
-            $identifier = "";
-        }
-            return $identifier;
+        return $this->manager->getRepository(Organism::class)->getDetailsOfOrganism($fennecId, $dbversion, $user);
     }
 }
