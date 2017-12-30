2afd30f4be8413e95cb6befc849bc34eeec40847
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-12-30 09:50:48 +0100

    Update service for getting traits of organism

--- src/AppBundle/API/Details/OrganismsWithTrait.php
+++ src/AppBundle/API/Details/OrganismsWithTrait.php
@@ -13,48 +13,24 @@ use Symfony\Component\HttpFoundation\ParameterBag;
  * Web Service.
  * Returns Organisms that posess a given trait
  */
-class OrganismsWithTrait extends Webservice
+class OrganismsWithTrait
 {
     const DEFAULT_LIMIT = 100;
 
+    private $manager;
+
     /**
-     * @param $query ParameterBag $query[type_cvterm_id] array of trait type_cvterm_id
-     * @param $user FennecUser|null
-     * @returns array of organisms accoring to a specific trait type
+     * OrganismsWithTrait constructor.
+     * @param $dbversion
      */
-    public function execute(ParameterBag $query, FennecUser $user = null)
+    public function __construct(DBVersion $dbversion)
     {
-        $db = $this->getManagerFromQuery($query)->getConnection();
-        $trait_type_id = $query->get('trait_type_id');
-        $limit = OrganismsWithTrait::DEFAULT_LIMIT;
-        if ($query->has('limit')) {
-            $limit = $query->get('limit');
-        }
-        
-        $query_get_organism_by_trait = <<<EOF
-(SELECT *
-    FROM organism, (SELECT DISTINCT fennec_id FROM trait_categorical_entry WHERE trait_type_id = :trait_type_id AND deletion_date IS NULL) AS ids
-    WHERE organism.fennec_id = ids.fennec_id)
-UNION 
-(SELECT *
-    FROM organism, (SELECT DISTINCT fennec_id FROM trait_numerical_entry WHERE trait_type_id = :trait_type_id AND deletion_date IS NULL) AS ids
-    WHERE organism.fennec_id = ids.fennec_id)
-LIMIT :limit
-EOF;
-        $stm_get_organism_by_trait = $db->prepare($query_get_organism_by_trait);
-        $stm_get_organism_by_trait->bindValue('trait_type_id', $trait_type_id);
-        $stm_get_organism_by_trait->bindValue('limit', $limit);
-        $stm_get_organism_by_trait->execute();
-        
-        $data = array();
-        
-        while ($row = $stm_get_organism_by_trait->fetch(PDO::FETCH_ASSOC)) {
-            $result = array();
-            $result['fennec_id'] = $row['fennec_id'];
-            $result['scientific_name'] = $row['scientific_name'];
-            $data[] = $result;
-        }
-        return $data;
-        
+        $this->manager = $dbversion->getEntityManager();
+    }
+
+
+    public function execute($trait_type_id)
+    {
+        return $this->manager->getRepository(Organism::class)->getOrganismByTrait($trait_type_id);
     }
 }
