aba718b9ced80d29c94c45d1976084e28067accf
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-01-02 11:30:27 +0100

    Implement function for getting values to a specific trait type

--- src/AppBundle/Repository/TraitTypeRepository.php
+++ src/AppBundle/Repository/TraitTypeRepository.php
@@ -16,4 +16,18 @@ class TraitTypeRepository extends EntityRepository
         $query = $this->getEntityManager()->createQuery('SELECT COUNT(tt.id) FROM AppBundle\Entity\TraitType tt');
         return $query->getSingleScalarResult();
     }
+
+    /**
+     * @param $trait_type_id
+     * @return array type, format, trait_type_id and ontology_url of specific trait
+     */
+    public function getInfo($trait_type_id){
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('t.id AS trait_type_id', 't.type', 't.ontologyUrl', 'IDENTITY(t.traitFormat) AS trait_format_id', 't.description', 't.unit')
+            ->from('AppBundle\Entity\TraitType', 't')
+            ->where('t.id = :trait_type_id')
+            ->setParameter('trait_type_id', $trait_type_id);
+        $query = $qb->getQuery();
+        return $query->getResult();
+    }
 }
