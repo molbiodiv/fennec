e40bfc5c49043616aefc3dc1f86abe8e054fcc2d
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-01-03 11:12:59 +0100

    Remove unused function from traits service

--- src/AppBundle/API/Details/Traits.php
+++ src/AppBundle/API/Details/Traits.php
@@ -62,73 +62,13 @@ class Traits
                 $result['number_of_organisms'] = $this->manager->getRepository(TraitNumericalEntry::class)->getNumberOfOrganisms($trait_type_id, $fennec_ids);
             }
         }
-        if ($query->has('include_citations') && $query->get('include_citations')){
-            $result['citations'] = $this->get_citations($trait_type_id, $fennec_ids, $format);
+        if ($include_citations) {
+            if ($format['format'] === 'categorical_free') {
+                $result['citations'] = $this->manager->getRepository(TraitCategoricalEntry::class)->getCitations($trait_type_id, $fennec_ids);
+            } else {
+                $result['citations'] = $this->manager->getRepository(TraitNumericalEntry::class)->getCitations($trait_type_id, $fennec_ids);
+            }
         }
         return $result;
-    }
-
-    /**
-     * @param $trait_type_id
-     * @param $fennec_ids
-     * @param $trait_format string - one of categorical|numerical
-     * @return array citations by fennec_id
-     */
-    private function get_citations($trait_type_id, $fennec_ids, $trait_format){
-        if ($fennec_ids !== null and count($fennec_ids) === 0){
-            return [];
-        }
-        $organism_constraint = $this->get_organism_constraint($fennec_ids);
-        $query_get_citations = $this->get_citation_query_for_format($trait_format, $organism_constraint);
-        $stm_get_citations= $this->db->prepare($query_get_citations);
-        if($fennec_ids !== null){
-            $stm_get_citations->execute(array_merge(array($trait_type_id), $fennec_ids));
-        } else {
-            $stm_get_citations->execute(array($trait_type_id));
-        }
-
-        $citations = [];
-        while ($row = $stm_get_citations->fetch(PDO::FETCH_ASSOC)) {
-            if(!array_key_exists($row['fennec_id'], $citations)){
-                $citations[$row['fennec_id']] = array();
-            }
-            $citations[$row['fennec_id']][] = [
-                "citation" => $row['citation'],
-                "value" => $row['value']
-            ];
-        }
-
-        return $citations;
-    }
-
-    /**
-     * @param $trait_format
-     * @param $organism_constraint
-     * @return string
-     */
-    private function get_citation_query_for_format($trait_format, $organism_constraint)
-    {
-        $query_get_citations = "";
-        if($trait_format === 'categorical'){
-            $query_get_citations = <<<EOF
-    SELECT fennec_id, value, citation
-    FROM trait_categorical_entry, trait_citation, trait_categorical_value
-    WHERE trait_categorical_entry.trait_type_id = ?
-    AND trait_citation_id = trait_citation.id
-    AND trait_categorical_value.id = trait_categorical_value_id
-    AND deletion_date IS NULL
-        {$organism_constraint}
-EOF;
-        } elseif($trait_format === 'numerical'){
-            $query_get_citations = <<<EOF
-    SELECT fennec_id, citation, value
-    FROM trait_numerical_entry, trait_citation 
-    WHERE trait_type_id = ?
-    AND trait_citation_id = trait_citation.id
-    AND deletion_date IS NULL
-        {$organism_constraint}
-EOF;
-        }
-        return $query_get_citations;
     }
 }
