82172e8ae9f10e93d55cf82ab708dd4ce56c92a1
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-01-04 16:00:24 +0100

    Remove obsolet function getTraits from traits service

--- src/AppBundle/API/Listing/Traits.php
+++ src/AppBundle/API/Listing/Traits.php
@@ -46,41 +46,4 @@ class Traits
         usort($data, function($a,$b){return $b['frequency'] - $a['frequency'];});
         return $data;
     }
-
-    /**
-     * @param $db
-     * @param $search
-     * @param $limit
-     * @param $trait_format
-     * @return array
-     */
-    private function get_traits($db, $search, $limit, $trait_format)
-    {
-        $query_get_traits = <<<EOF
-SELECT trait_type.type AS name, trait_type_id, count
-    FROM 
-        (
-            SELECT trait_type_id, count(trait_type_id) AS count
-EOF;
-        $query_get_traits .= " FROM trait_".$trait_format."_entry WHERE deletion_date IS NULL GROUP BY trait_type_id";
-
-        $query_get_traits .= <<<EOF
-        ) AS trait_entry, trait_type
-            WHERE trait_type.id=trait_type_id AND trait_type.type ILIKE :search ORDER BY count DESC LIMIT :limit
-EOF;
-        $stm_get_traits = $db->prepare($query_get_traits);
-        $stm_get_traits->bindValue('search', $search);
-        $stm_get_traits->bindValue('limit', $limit);
-        $stm_get_traits->execute();
-
-        $data = array();
-        while ($row = $stm_get_traits->fetch(PDO::FETCH_ASSOC)) {
-            $result = array();
-            $result['name'] = $row['name'];
-            $result['trait_type_id'] = $row['trait_type_id'];
-            $result['frequency'] = $row['count'];
-            $data[] = $result;
-        }
-        return $data;
-    }
 }
