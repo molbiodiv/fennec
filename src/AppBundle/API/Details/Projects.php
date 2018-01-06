273f5895175ea48d1afe79fc50a3f4ad8d68f14f
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-01-06 20:14:41 +0100

    Add constructor to details of projects service

--- src/AppBundle/API/Details/Projects.php
+++ src/AppBundle/API/Details/Projects.php
@@ -5,15 +5,30 @@ namespace AppBundle\API\Details;
 use AppBundle\API\Webservice;
 use AppBundle\Entity\WebuserData;
 use AppBundle\Entity\FennecUser;
+use AppBundle\Service\DBVersion;
 use Symfony\Component\HttpFoundation\ParameterBag;
 
 /**
  * Web Service.
  * Returns a project according to the project ID.
  */
-class Projects extends Webservice
+class Projects
 {
     const PROJECT_NOT_FOUND_FOR_USER = "Error: At least one project could not be found for the current user.";
+    const ERROR_NOT_LOGGED_IN = "Error: You are not logged in.";
+
+    private $manager;
+
+    /**
+     * Projects constructor.
+     * @param $dbversion
+     */
+    public function __construct(DBVersion $dbversion)
+    {
+        $this->manager = $dbversion->getEntityManager();
+    }
+
+
     /**
     * @inheritdoc
     * @returns array $result
