b75116b88506b5a6c474cc67de90c60407fd1198
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-01-06 20:16:18 +0100

    Update execute function in listing of projects service

--- src/AppBundle/API/Listing/Projects.php
+++ src/AppBundle/API/Listing/Projects.php
@@ -40,19 +40,19 @@ class Projects
     {
         $result = array('data' => array());
         if ($user == null) {
-            $result['error'] = Webservice::ERROR_NOT_LOGGED_IN;
+            $result['error'] = Projects::ERROR_NOT_LOGGED_IN;
         } else {
-            $projects = $user->getData();
+            $userId = $user->getId();
+            $projects = $this->manager->getRepository(WebuserData::class)->getDataForUser($userId);
             foreach ($projects as $p) {
-                /** @var WebuserData $p */
                 $project = array();
-                $project['internal_project_id'] = $p->getWebuserDataId();
-                $data = $p->getProject();
+                $project['internal_project_id'] = $p['webuserDataId'];
+                $data = $p['project'];
                 $project['id'] = $data['id'];
-                $project['import_date'] = $p->getImportDate()->format('Y-m-d H:i:s');
+                $project['import_date'] = $p['importDate']->format('Y-m-d H:i:s');
                 $project['rows'] = $data['shape'][0];
                 $project['columns'] = $data['shape'][1];
-                $project['import_filename'] = $p->getImportFilename();
+                $project['import_filename'] = $p['importFilename'];
                 $result['data'][] = $project;
             }
         }
