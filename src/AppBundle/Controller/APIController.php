237215fa703f3c17688d7ce122e8f8a35674bbe9
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-01-06 10:57:20 +0100

    Remove obsolet call of webservice class in APIController

--- src/AppBundle/Controller/APIController.php
+++ src/AppBundle/Controller/APIController.php
@@ -18,8 +18,13 @@ class APIController extends Controller
      * @Route("/api/{namespace}/{classname}", name="api", options={"expose" = true})
      */
     public function apiAction(Request $request, $namespace, $classname){
+        $serviceNamespace = 'AppBundle\\API\\' . ucfirst($namespace);
+        $class = $serviceNamespace . '\\' . ucfirst($classname);
+        if (!class_exists($class)) {
+            throw new Exception("Could not find class: ".$namespace."\\".$class);
+        }
         try{
-            $service = $this->get('app.api.webservice')->factory($namespace, $classname);
+            $service = $this->container->get($class);
         } catch (Exception $e){
             throw $this->createNotFoundException('Webservice not found in the API');
         }
@@ -28,7 +33,7 @@ class APIController extends Controller
         if($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')){
             $user = $this->get('security.token_storage')->getToken()->getUser();
         }
-        $result = $service->execute($queryData, $user);
+        $result = $service->execute($user);
         $response = $this->json($result);
         $response->headers->set('Access-Control-Allow-Origin', '*');
         return $response;
