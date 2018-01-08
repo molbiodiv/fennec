3b8e735deaf5d4a9d838462244deac694823a81a
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-01-08 13:58:47 +0100

    Add attribute and constructor to service for editing projects and update execute function

--- src/AppBundle/API/Edit/UpdateProject.php
+++ src/AppBundle/API/Edit/UpdateProject.php
@@ -4,34 +4,49 @@ namespace AppBundle\API\Edit;
 
 
 use AppBundle\API\Webservice;
+use AppBundle\AppBundle;
 use AppBundle\Entity\FennecUser;
+use AppBundle\Entity\WebuserData;
+use AppBundle\Service\DBVersion;
 use Symfony\Component\HttpFoundation\ParameterBag;
 
-class UpdateProject extends Webservice
+class UpdateProject
 {
+    private $manager;
+
+    /**
+     * UpdateProject constructor.
+     * @param $dbversion
+     */
+    public function __construct(DBVersion $dbversion)
+    {
+        $this->manager = $dbversion->getEntityManager();
+    }
+
+
     /**
      * @inheritdoc
      */
-    public function execute(ParameterBag $query, FennecUser $user = null)
+    public function execute(FennecUser $user = null)
     {
-        $em = $this->getManagerFromQuery($query);
-        if(!$query->has('biom') || !$query->has('project_id')){
+        $project_id = $_REQUEST['project_id'];
+        $biom = $_REQUEST['biom'];
+        if($biom === null || $project_id === null){
             return array('error' => 'Missing parameter "biom" or "project_id"');
         }
         if($user == null){
             return array('error' => 'User not logged in');
         }
-        $webuser = $user;
-        if($webuser === null){
+        if($user === null){
             return array('error' => 'Could not update project. Not found for user.');
         }
-        $project = $em->getRepository('AppBundle:WebuserData')->findOneBy(array('webuser' => $webuser, 'webuserDataId' => $query->get('project_id')));
+        $project = $this->manager->getRepository(WebuserData::class)->findOneBy(array('webuser' => $user, 'webuserDataId' => $project_id));
         if($project === null){
             return array('error' => 'Could not update project. Not found for user.');
         }
-        $project->setProject(json_decode($query->get('biom'), true));
-        $em->persist($project);
-        $em->flush();
+        $project->setProject(json_decode($biom, true));
+        $this->manager->persist($project);
+        $this->manager->flush();
         return array('error' => null);
     }
 }
