4688e26dd444a456cc74f45c4c8149309ef8bd13
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-01-10 14:21:29 +0100

    Implement several api functions

--- src/AppBundle/Controller/APIController.php
+++ src/AppBundle/Controller/APIController.php
@@ -51,6 +51,70 @@ class APIController extends Controller
         return $response;
     }
 
+    /**
+     * @param Request $request
+     * @return Response $response
+     * @Route("/api/listing/projects/", name="api_listing_projects", options={"expose"=true})
+     */
+    public function listingProjectsAction(Request $request){
+        $projects = $this->container->get(Listing\Projects::class);
+        $user = $this->getFennecUser();
+        $result = $projects->execute($user);
+        $response = $this->json($result);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response $response
+     * @Route("/api/delete/projects/", name="api_delete_projects", options={"expose"=true})
+     */
+    public function deleteProjectsAction(Request $request){
+        $projectId = $request->query->get('projectId');
+        $deleteProjects = $this->container->get(Delete\Projects::class);
+        $user = $this->getFennecUser();
+        $result = $deleteProjects->execute($user, $projectId);
+        $response = $this->json($result);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response $response
+     * @Route("/api/upload/projects/", name="api_upload_projects", options={"expose"=true})
+     */
+    public function uploadProjectsAction(Request $request){
+        $uploadProjects = $this->container->get(Upload\Projects::class);
+        $user = $this->getFennecUser();
+        $result = $uploadProjects->execute($user);
+        $response = $this->json($result);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response $response
+     * @Route("/api/upload/projects/", name="api_details_traits_of_organisms", options={"expose"=true})
+     */
+    public function detailsTraitsOfOrganismsAction(Request $request){
+        $traitDetails = $this->container->get(Details\TraitsOfOrganisms::class);
+        $result = $traitDetails->execute($request->query->get('fennecIds'));
+        return $this->createResponse($result);
+    }
+/**
+     * @param Request $request
+     * @return Response $response
+     * @Route("/api/details/traitOfProject/", name="api_details_trait_of_project'", options={"expose"=true})
+     */
+    public function detailsTraitOfProjectAction(Request $request){
+        $traitDetails = $this->container->get(Details\TraitOfProject::class);
+        $result = $traitDetails->execute($request->query->get('fennecIds'));
+        return $this->createResponse($result);
+    }
+
     private function getFennecUser(){
         $user = null;
         if($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')){
