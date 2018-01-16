b7294df20523296050d39222436b7b848bb05da8
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-01-16 13:22:57 +0100

    Make test fir scinames pass

--- tests/AppBundle/API/Listing/ScinamesTest.php
+++ tests/AppBundle/API/Listing/ScinamesTest.php
@@ -31,9 +31,8 @@ class ScinamesTest extends WebserviceTestCase
     }
     public function testExecute()
     {
-        $organisms = $this->webservice->factory('listing', 'scinames');
-        $parameterBag = new ParameterBag(array('dbversion' => $this->default_db, 'ids' => [5,43,234,6432,32421,120121]));
-        $results = $organisms->execute($parameterBag, null);
+        $ids = [5,43,234,6432,32421,120121];
+        $results = $this->listingScinames->execute($ids);
         $expected = array(
             "5" => "Chlorophyta",
             "43" => "green alga KS3/2",
@@ -47,9 +46,8 @@ class ScinamesTest extends WebserviceTestCase
 
     public function testExecuteNoIds()
     {
-        $organisms = $this->webservice->factory('listing', 'scinames');
-        $parameterBag = new ParameterBag(array('dbversion' => $this->default_db));
-        $results = $organisms->execute($parameterBag, null);
+        $ids = null;
+        $results = $this->listingScinames->execute($ids);
         $expected = array();
         $this->assertEquals($expected, $results);
     }
