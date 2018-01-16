622edb500b06a55cfd0b43e3f847dcf7cb4b1bf5
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-01-16 15:06:53 +0100

    Update test function for testing mapping of full organism names

--- tests/AppBundle/API/Mapping/FullByOrganismNameTest.php
+++ tests/AppBundle/API/Mapping/FullByOrganismNameTest.php
@@ -4,13 +4,34 @@ namespace Tests\AppBundle\API\Mapping;
 
 use Symfony\Component\HttpFoundation\ParameterBag;
 use Tests\AppBundle\API\WebserviceTestCase;
+use AppBundle\API\Mapping;
 
 class FullByOrganismNameTest extends WebserviceTestCase
 {
+    private $em;
+    private $fullByOrganismName;
+    public function setUp()
+    {
+        $kernel = self::bootKernel();
+
+        $this->em = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager('test');
+        $this->fullByOrganismName = $kernel->getContainer()->get(Mapping\FullByOrganismName::class);
+
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        $this->em->close();
+        $this->em = null; // avoid memory leaks
+    }
+
     public function testExecute()
     {
-        $service = $this->webservice->factory('mapping', 'fullByOrganismName');
-        $result = $service->execute(new ParameterBag(array('dbversion' => $this->default_db)), null);
+        $result = $this->fullByOrganismName->execute();
         $this->assertEquals(195203, count($result));
         $this->assertEquals(10, $result['Dunaliella tertiolecta']);
         $this->assertEquals([213,196104], $result['Onoclea sensibilis']);
