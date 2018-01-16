66707d73d3f160e1354eb5d78aa647e217809280
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-01-16 15:33:14 +0100

    Make test for uploading projects pass

--- tests/AppBundle/API/Upload/ProjectsTest.php
+++ tests/AppBundle/API/Upload/ProjectsTest.php
@@ -122,7 +122,8 @@ class ProjectsTest extends WebserviceTestCase
         $this->assertEquals($expected, $results);
     }
 
-    public function testUploadBiom(){
+    public function testUploadBiom()
+    {
         $user = new FennecUser();
         $user->setUsername('UploadProjectTestUser');
         $user->setEmail('UploadProjectTestUser@test.de');
@@ -136,30 +137,15 @@ class ProjectsTest extends WebserviceTestCase
             )
         );
         $results = $this->uploadProjects->execute($user);
-        $expected = array("files"=>array(array("name" => "simpleBiom.json", "size" => 1067, "error" => null)));
+        $expected = array("files" => array(array("name" => "simpleBiom.json", "size" => 1067, "error" => null)));
         $this->assertEquals($expected, $results);
-        $jsonContent = file_get_contents($_FILES[0]['tmp_name']);
-        $db = $this->container->get('app.orm')->getManagerForVersion($default_db)->getConnection();
-        $constant = 'constant';
-        $query_get_project_from_db = <<<EOF
-SELECT project, import_filename
-    FROM webuser_data WHERE webuser_id =
-        (SELECT webuser_id FROM webuser WHERE oauth_provider_id =
-            (SELECT oauth_provider_id FROM oauth_provider
-                WHERE provider = '{$constant('Tests\AppBundle\API\Upload\ProjectsTest::PROVIDER')}'
-            )
-            AND oauth_id = '{$constant('Tests\AppBundle\API\Upload\ProjectsTest::USERID')}'
-        )
-        AND project::jsonb = '{$jsonContent}'::jsonb
-EOF;
-        $stm_get_project_from_db = $db->prepare($query_get_project_from_db);
-        $stm_get_project_from_db->execute();
-        //this test is commented out according to issue #108
-//        $this->assertEquals(1, $stm_get_project_from_db->rowCount());
-//        $result = $stm_get_project_from_db->fetch(\PDO::FETCH_ASSOC);
-//        $this->assertEquals('simpleBiom.json', $result['import_filename']);
+    }
 
-        // Test for success returned by simple biom file in hdf5 format
+    public function testUploadSimpleBiomInHdf5()
+    {
+        $user = new FennecUser();
+        $user->setUsername('UploadProjectTestUser');
+        $user->setEmail('UploadProjectTestUser@test.de');
         copy(__DIR__ . '/testFiles/simpleBiom.hdf5', __DIR__ . '/testFiles/simpleBiom.hdf5.backup');
         $_FILES = array(
             array(
@@ -170,15 +156,16 @@ EOF;
                 'error' => 0
             )
         );
-        $results = $service->execute(
-            new ParameterBag(array('dbversion' => $default_db)),
-            $user
-        );
-        $expected = array("files"=>array(array("name" => "simpleBiom.hdf5", "size" => 33840, "error" => null)));
+        $results = $this->uploadProjects->execute($user);
+        $expected = array("files" => array(array("name" => "simpleBiom.hdf5", "size" => 33840, "error" => null)));
         $this->assertEquals($expected, $results);
         rename(__DIR__ . '/testFiles/simpleBiom.hdf5.backup', __DIR__ . '/testFiles/simpleBiom.hdf5');
+    }
 
-        // Test for success returned by plain otu table file
+    public function testUploadOtuTable(){
+        $user = new FennecUser();
+        $user->setUsername('UploadProjectTestUser');
+        $user->setEmail('UploadProjectTestUser@test.de');
         copy(__DIR__ . '/testFiles/otuTable.tsv', __DIR__ . '/testFiles/otuTable.tsv.backup');
         $_FILES = array(
             array(
@@ -189,10 +176,7 @@ EOF;
                 'error' => 0
             )
         );
-        $results = $service->execute(
-            new ParameterBag(array('dbversion' => $default_db)),
-            $user
-        );
+        $results = $this->uploadProjects->execute($user);
         $expected = array("files"=>array(array("name" => "otuTable.tsv", "size" => 67, "error" => null)));
         $this->assertEquals($expected, $results);
         rename(__DIR__ . '/testFiles/otuTable.tsv.backup', __DIR__ . '/testFiles/otuTable.tsv');
