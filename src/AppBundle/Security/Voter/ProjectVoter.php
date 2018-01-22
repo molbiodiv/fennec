7a88a6ddc7649fc2988e0a7e213467cff414a2ca
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-01-22 16:37:35 +0100

    Implement custom voter

--- src/AppBundle/Security/Voter/ProjectVoter.php
+++ src/AppBundle/Security/Voter/ProjectVoter.php
@@ -8,8 +8,67 @@
 
 namespace AppBundle\Security\Voter;
 
+use AppBundle\Entity\WebuserData;
+use AppBundle\Entity\FennecUser;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
-class ProjectVoter
+class ProjectVoter extends Voter
 {
+    // these strings are just invented: you can use anything
+    const VIEW = 'view';
+    const EDIT = 'edit';
+    const OWNER = 'owner';
 
+    protected function supports($attribute, $subject)
+    {
+        // if the attribute isn't one we support, return false
+        if (!in_array($attribute, array(self::VIEW, self::EDIT))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof FennecUser) {
+            // the user must be logged in; if not, deny access
+            return false;
+        }
+
+        // you know $subject is a Post object, thanks to supports
+        /** @var WebuserData $post */
+        $post = $subject;
+
+        switch ($attribute) {
+            case self::VIEW:
+                return $this->canView($post, $user);
+            case self::EDIT:
+                return $this->canEdit($post, $user);
+        }
+
+        throw new \LogicException('This code should not be reached!');
+    }
+
+    private function canView(WebuserData $post, FennecUser $user)
+    {
+        // if they can edit, they can view
+        if ($this->canEdit($post, $user)) {
+            return true;
+        }
+
+        // the Post object could have, for example, a method isPrivate()
+        // that checks a boolean $private property
+        return !$post->isPrivate();
+    }
+
+    private function canEdit($post, $user)
+    {
+        // this assumes that the data object has a getOwner() method
+        // to get the entity of the user who owns this data object
+        return $user === $post[''];
+    }
 }
