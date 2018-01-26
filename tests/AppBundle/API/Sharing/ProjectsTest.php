e93bf56ee1b5492c9af15f58c29158e026ab2949
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-01-26 13:41:09 +0100

    Make first test case for testing sharing of projects pass

--- tests/AppBundle/API/Sharing/ProjectsTest.php
+++ tests/AppBundle/API/Sharing/ProjectsTest.php
@@ -2,11 +2,20 @@
 
 use Tests\AppBundle\API\WebserviceTestCase;
 use AppBundle\API\Sharing;
+use AppBundle\API\Upload;
+use AppBundle\Entity\FennecUser;
+use AppBundle\Entity\Permissions;
 
 class ProjectsTest extends WebserviceTestCase
 {
     private $em;
     private $sharingProjects;
+    private $uploadProjects;
+
+    const NICKNAME = 'SharingProjectsTestUser';
+    const EMAIL = 'SharingProjectsTestUser@bla.de';
+    const ANOTHER_NICKNAME = 'AnotherSharingProjectsTestUser';
+    const ANOTHER_EMAIL = 'AnotherSharingProjectsTestUser@bla.de';
 
     public function setUp()
     {
@@ -16,6 +25,7 @@ class ProjectsTest extends WebserviceTestCase
             ->get('doctrine')
             ->getManager('test');
         $this->sharingProjects = $kernel->getContainer()->get(Sharing\Projects::class);
+        $this->uploadProjects = $kernel->getContainer()->get(Upload\Projects::class);
     }
 
     public function tearDown()
@@ -24,5 +34,48 @@ class ProjectsTest extends WebserviceTestCase
 
         $this->em->close();
         $this->em = null; // avoid memory leaks
+    }
+
+    public function testReadOnlySharingOfProject(){
+        $user = new FennecUser();
+        $user->setUsername(ProjectsTest::NICKNAME);
+        $user->setEmail(ProjectsTest::EMAIL);
+        $anotherUser = new FennecUser();
+        $anotherUser->setUsername(ProjectsTest::ANOTHER_NICKNAME);
+        $anotherUser->setEmail(ProjectsTest::ANOTHER_EMAIL);
+        $_FILES = array(
+            array(
+                'name' => 'simpleBiom.json',
+                'type' => 'text/plain',
+                'size' => 1067,
+                'tmp_name' => __DIR__ . '/testFiles/simpleBiom.json',
+                'error' => 0
+            )
+        );
+        $results = $this->uploadProjects->execute($user);
+        $_FILES = array(
+            array(
+                'name' => 'anotherSimpleBiom.json',
+                'type' => 'text/plain',
+                'size' => 1067,
+                'tmp_name' => __DIR__ . '/testFiles/simpleBiom.json',
+                'error' => 0
+            )
+        );
+        $results = $this->uploadProjects->execute($anotherUser);
+        $permission = $this->em->getRepository(Permissions::class)->findOneBy(array(
+            'webuser' => $user
+        ));
+        $anotherPermission = $this->em->getRepository(Permissions::class)->findOneBy(array(
+            'webuser' => $anotherUser
+        ));
+        $this->assertEquals('owner', $permission->getPermission());
+        $this->assertEquals('owner', $anotherPermission->getPermission());
+        $this->sharingProjects->execute(ProjectsTest::EMAIL, )
+
+    }
+
+    public function testReadAndWriteSharingOfProject(){
+
     }
 }
