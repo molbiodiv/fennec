541c83c4bc9352544fd532d5990970ea02b68f58
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-02-01 15:48:26 +0100

    Remove obsolet services for webservice class

---
+++ src/AppBundle/Validator/Constraints/IsValidEmailForUserValidator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sonja
+ * Date: 01.02.18
+ * Time: 15:11
+ */
+
+namespace AppBundle\Validator\Constraints;
+
+
+class IsValidEmailForUserValidator
+{
+
+}
