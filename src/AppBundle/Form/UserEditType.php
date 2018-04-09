b9ea43f90260d9e09fce4a172db1c9d60da4f67d
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-04-09 15:05:51 +0200

    Rename class

--- src/AppBundle/Form/UserEditType.php
+++ src/AppBundle/Form/UserEditType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class RegistrationFormType extends AbstractType
+class UserEditType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
