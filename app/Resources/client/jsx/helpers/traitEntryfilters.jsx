8ab4db4f50b2a83d43a271dbea10065bf655c679
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2018-04-13 12:39:12 +0200

    Add filter functionality for traitEntries

--- app/Resources/client/jsx/helpers/traitEntryfilters.jsx
+++ app/Resources/client/jsx/helpers/traitEntryfilters.jsx
@@ -1,20 +1,45 @@
 export class TraitEntryFilter {
     fullData
-    filter = {
-        provider: [],
-        user: []
-    }
+    _filter
 
     constructor(fullData) {
         this.fullData = fullData
-    }
-
-    filter(){
-       let filteredData = this.fullData
-        if(this.filter.provider.length > 0){
-           filteredData = filteredData.filter()
+        this.filter = {
+            providerBlacklist: [],
+            userBlacklist: []
         }
-       return filteredData
     }
 
+    get filter(){
+        return this._filter
+    }
+
+    set filter(filter){
+        this._filter = filter
+    }
+
+    applyFilter(){
+        let filteredData = this.fullData
+        if(this.filter.providerBlacklist.length > 0){
+            filteredData = filteredData.map(traitType => {
+                return {
+                    traitType: traitType.traitType,
+                    entries: traitType.entries.filter(e => !this.filter.providerBlacklist.includes(e.provider))
+                }
+            })
+        }
+        if(this.filter.userBlacklist.length > 0  && !this.filter.providerBlacklist.includes('userImport')){
+            filteredData = filteredData.map(traitType => {
+                return {
+                    traitType: traitType.traitType,
+                    entries: traitType.entries.filter(e => !this.filter.userBlacklist.includes(e.user))
+                }
+            })
+        }
+        filteredData = filteredData.filter(x => x.entries.length > 0)
+        return filteredData
+    }
 }
+
+// export globally
+window.TraitEntryFilter = TraitEntryFilter;
