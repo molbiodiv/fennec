5e7ac9a34e57d2ec414bd6fbd5d79054b633ad4f
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-04-20 14:47:07 +0200

    Check which provider is checked

--- app/Resources/client/jsx/project/details/trait.jsx
+++ app/Resources/client/jsx/project/details/trait.jsx
@@ -214,19 +214,19 @@ function applyTraitEntryFilter(){
     let userBlacklist = [user]
     let providerBlacklist = []
     let traitFormatBlacklist = []
-    if($('#trait-filter-by-ncbi').checked){
+    if($('#trait-filter-by-ncbi').prop("checked")){
         providerBlacklist.push($('#trait-filter-by-ncbi').val())
     }
-    if($('#trait-filter-by-iucn').checked){
+    if($('#trait-filter-by-iucn').prop("checked")){
         providerBlacklist.push($('#trait-filter-by-iucn').val())
     }
-    if($('#trait-filter-by-eol').checked){
+    if($('#trait-filter-by-eol').prop("checked")){
         providerBlacklist.push($('#trait-filter-by-eol').val())
     }
-    if($('#trait-filter-by-eppo').checked){
+    if($('#trait-filter-by-eppo').prop("checked")){
         providerBlacklist.push($('#trait-filter-by-eppo').val())
     }
-    if($('#trait-filter-by-leda').checked){
+    if($('#trait-filter-by-leda').prop("checked")){
         providerBlacklist.push($('#trait-filter-by-leda').val())
     }
     if(format != "none"){
