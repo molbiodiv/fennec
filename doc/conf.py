8d15916b6a3651e9ea3db1aef4bcfaf11c945395
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2018-07-16 11:33:36 +0200

    Add sphinx theme only if not on rtd

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
@@ -119,19 +120,19 @@ todo_include_todos = False
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = ['_themes']
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
-# html_theme_options = {}
+#html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['_themes']
+# html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
